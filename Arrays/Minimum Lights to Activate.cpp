77bd5b5f4b605779cc78114c76415c174c54457d
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-01-08 17:32:26 +0530

    Create Minimum Lights to Activate.cpp

---
+++ Arrays/Minimum Lights to Activate.cpp
@@ -0,0 +1,29 @@
+int Solution::solve(vector<int> &A, int B) {
+    
+    int curr=0,count=0;
+    int n=A.size();
+    while(curr<n){
+        int prev,next;
+        prev=curr-B+1;
+        next=curr+B-1;
+        
+        if(prev<0){
+            prev=0;
+        }
+        
+        while(next>=prev){
+            if(A[next]) //Finding the right most bulb
+            break;
+            
+            next--;
+        }
+        
+        if(next>=prev){
+            curr=next+B;  //Till next+B-1 there will be light 
+            count++;
+        }
+        else
+            return -1; //No unfaulty bulb available
+    }
+    return count;
+}
