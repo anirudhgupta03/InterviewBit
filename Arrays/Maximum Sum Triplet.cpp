a1454a1e1cdc2625fc6e9642a13d60c886a142be
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-01-08 12:42:30 +0530

    Update Maximum Sum Triplet.cpp

--- Arrays/Maximum Sum Triplet.cpp
+++ Arrays/Maximum Sum Triplet.cpp
@@ -2,11 +2,11 @@
 //Best and efficient approach is use the concept of maximum suffix-array and binary search.
 
 //For finding maximum number greater than given number beyond it, we can maintain a maximum suffix-array array such that for any number(suffix[i]) it would 
-contain maximum number from index i, i+1, … N-1. Suffix array can be calculated in O(N) time.
+//contain maximum number from index i, i+1, … N-1. Suffix array can be calculated in O(N) time.
 
 //For finding maximum number smaller than the given number preceding it, we can maintain a sorted list of numbers before a given number such we can 
-simply perform a binary search to find a number which is just smaller than the given number. In C++ language, we can perform this by using 
-set associative container of STL library.
+//simply perform a binary search to find a number which is just smaller than the given number. In C++ language, we can perform this by using 
+//set associative container of STL library.
 
 //Time complexity: O(NlogN)
 //Auxiliary space: O(N)
