bb55be8522e2bac6fe6fd5786a7353b4ad2cbb81
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-01-12 19:33:29 +0530

    Create Prime Numbers.cpp

---
+++ Math/Prime Numbers.cpp
@@ -0,0 +1,26 @@
+vector<int> Solution::sieve(int A) {
+
+    bool primes[A+1];
+    
+    memset(primes,true,sizeof(primes));
+    
+    for(int p = 2; p*p <= A; p++){
+        
+        if(primes[p]==true){
+            
+            for(int i= p*p ;i <= A;i+=p){
+                primes[p]=false;
+            }
+        }
+    }
+    
+    vector<int> v;
+    
+    for(int p=2;p<=A;p++){
+        if(primes[p]==true){
+            v.push_back(p);
+        }
+    }
+    
+    return v;
+}
