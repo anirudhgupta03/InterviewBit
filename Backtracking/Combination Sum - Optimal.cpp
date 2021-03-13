89646d0c03ba4cfd2d5d3109a7f61ecfbaa7d1e9
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-03-13 13:20:49 +0530

    Create Combination Sum - Optimal.cpp

---
+++ Backtracking/Combination Sum - Optimal.cpp
@@ -0,0 +1,45 @@
+set<vector<int>> s;
+
+void solve(vector<int> &A,vector<int> &v, int B, int ind){
+    
+    if(ind == A.size()){
+        if(B == 0){
+            s.insert(v);
+        }
+        return;
+    }
+    else if(B == 0){
+        s.insert(v);
+        return;
+    }
+    
+    if(A[ind] <= B){
+        solve(A,v,B,ind+1);
+        v.push_back(A[ind]);
+        solve(A,v,B-A[ind],ind);
+        v.pop_back();
+    }
+    else{
+        solve(A,v,B,ind+1);
+    }
+}
+vector<vector<int> > Solution::combinationSum(vector<int> &A, int B) {
+    
+    sort(A.begin(),A.end());
+    vector<int> v;
+    
+    vector<vector<int>> vec;
+    
+    int ind = 0;
+    
+    solve(A,v,B,ind);
+    
+    set<vector<int>> :: iterator it;
+    
+    for(it = s.begin(); it!=s.end(); it++){
+        vec.push_back(*it);
+    }
+    s.clear();
+    sort(vec.begin(),vec.end());
+    return vec;
+}
