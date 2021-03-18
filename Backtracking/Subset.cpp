0a5369f460cb98774bac6f50f46ad4ea1ca4044b
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-03-18 14:07:54 +0530

    Create Subset.cpp

---
+++ Backtracking/Subset.cpp
@@ -0,0 +1,29 @@
+void solve(vector<int> &s, vector<vector<int>> &v, vector<int> &A, int ind){
+    
+    if(ind == A.size()){
+        v.push_back(s);
+        return;
+    }
+    
+    solve(s, v, A, ind+1);
+    
+    s.push_back(A[ind]);
+    
+    solve(s, v, A, ind+1);
+    
+    s.pop_back();
+}
+vector<vector<int> > Solution::subsets(vector<int> &A) {
+    
+    sort(A.begin(),A.end());
+    
+    vector<int> s;
+    
+    vector<vector<int>> v;
+    
+    solve(s,v,A,0);
+    
+    sort(v.begin(),v.end());
+    
+    return v;
+}
