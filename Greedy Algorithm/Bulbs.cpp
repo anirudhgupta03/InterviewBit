dbaa0cf93dc076e715de9d5ac12f930a80330a4f
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-03-23 14:19:42 +0530

    Create Bulbs.cpp

---
+++ Greedy Algorithm/Bulbs.cpp
@@ -0,0 +1,26 @@
+int Solution::bulbs(vector<int> &A) {
+    
+    int n = A.size();
+    
+    if(n == 0){
+        return 0;
+    }
+    
+    int count = 0;
+    
+    for(int i = 0; i < A.size(); i++){
+        
+        if(count % 2 == 0){
+            if(A[i] == 0){
+                count++;
+            }
+        }
+        else{
+            A[i] = 1 - A[i];
+            if(A[i] == 0){
+                count++;
+            }
+        }
+    }
+    return count;
+}
