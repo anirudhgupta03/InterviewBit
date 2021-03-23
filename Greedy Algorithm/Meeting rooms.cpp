31816a130cc8ecb78ea268b9a09072214f9dd4bf
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-03-23 16:06:26 +0530

    Create Meeting rooms.cpp

---
+++ Greedy Algorithm/Meeting rooms.cpp
@@ -0,0 +1,33 @@
+int Solution::solve(vector<vector<int> > &A) {
+
+    int n = A.size();
+    
+    if(n == 0){
+        return 0;
+    }
+    int start[n], end[n];
+    
+    for(int i = 0; i < A.size(); i++){
+        start[i] = A[i][0];
+        end[i] = A[i][1];
+    }
+    sort(start,start+n);
+    sort(end,end+n);
+    
+    int res = 1;
+    
+    int i = 1, j = 0;
+    
+    while(i < n && j < n){
+        
+        if(end[j] <= start[i]){
+            i++;
+            j++;
+        }
+        else{
+            res++;
+            i++;
+        }
+    }
+    return res;
+}
