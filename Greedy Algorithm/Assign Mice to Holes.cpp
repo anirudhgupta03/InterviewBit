11aca3eab0c0532a06a30de569450e543e8e17c7
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-03-24 11:49:45 +0530

    Create Assign Mice to Holes.cpp

---
+++ Greedy Algorithm/Assign Mice to Holes.cpp
@@ -0,0 +1,13 @@
+int Solution::mice(vector<int> &A, vector<int> &B) {
+    
+    sort(A.begin(),A.end());
+    sort(B.begin(),B.end());
+    
+    int ans = INT_MIN;
+    
+    for(int i = 0; i < A.size(); i++){
+        ans = max(ans,abs(A[i]-B[i]));
+    }
+    
+    return ans;
+}
