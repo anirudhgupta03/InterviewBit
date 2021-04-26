3f978b331ee317dc9b381dfa9db1013c335c2338
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-04-26 23:08:29 +0530

    Update Subset Sum Problem! - Bottom Up Approach.cpp

--- Dynamic Programming/Subset Sum Problem! - Bottom Up Approach.cpp
+++ Dynamic Programming/Subset Sum Problem! - Bottom Up Approach.cpp
@@ -1,17 +1,18 @@
 int Solution::solve(vector<int> &A, int B) {
     
-    int dp[A.size() + 1][B + 1];
+    int n = A.size();
+    int dp[n+1][B+1];
     
-    for(int j = 0; j < B + 1; j++){
+    for(int j = 1; j <= B; j++){
         dp[0][j] = 0;
     }
     
-    for(int i = 0; i < A.size() + 1; i++){
+    for(int i = 0; i <= n; i++){
         dp[i][0] = 1;
     }
     
-    for(int i = 1; i < A.size() + 1; i++){
-        for(int j = 1; j < B + 1; j++){
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= B; j++){
             
             if(A[i-1] <= j){
                 dp[i][j] = dp[i-1][j-A[i-1]] || dp[i-1][j];
@@ -22,5 +23,7 @@ int Solution::solve(vector<int> &A, int B) {
         }
     }
     
-    return dp[A.size()][B];
+    return dp[n][B];
 }
+//Time Complexity: O(n*B)
+//Space Complexity: O(n*B)
