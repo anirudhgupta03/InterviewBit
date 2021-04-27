c5a037ed0169ef6005be4796020b500f09d20c6f
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-04-27 15:19:51 +0530

    Create Count Element Occurence.cpp

---
+++ Binary Search/Count Element Occurence.cpp
@@ -0,0 +1,47 @@
+int Solution::findCount(const vector<int> &A, int B) {
+    
+    int start = -1, n = A.size();
+    
+    int lo = 0, hi = n - 1;
+    
+    while(lo <= hi){
+        
+        int mid = (lo + hi)/2;
+        
+        if(A[mid] == B){
+            start = mid;
+            hi = mid - 1;
+        }
+        else if(A[mid] < B){
+            lo = mid + 1;
+        }
+        else if(A[mid] > B){
+            hi = mid - 1;
+        }
+    }
+    
+    if(start == -1){
+        return 0;
+    }
+    
+    int end = -1;
+    lo = 0, hi = n -1;
+    
+    while(lo <= hi){
+        
+        int mid = (lo + hi)/2;
+        
+        if(A[mid] == B){
+            end = mid;
+            lo = mid + 1;
+        }
+        else if(A[mid] < B){
+            lo = mid + 1;
+        }
+        else{
+            hi = mid - 1;
+        }
+    }
+    
+    return end - start + 1;
+}
