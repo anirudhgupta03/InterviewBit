3733c91f1c5a7ce795c85a79cae5dcf1e4d8875a
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-05-26 13:26:29 +0530

    Create An Increment Problem.cpp

---
+++ Hashing/An Increment Problem.cpp
@@ -0,0 +1,37 @@
+vector<int> Solution::solve(vector<int> &A) {
+    
+    unordered_map<int,vector<int>> umap;  //Unordered Map to store the indexes where an element is present
+    
+    vector<int> res;
+    
+    for(int i = 0; i < A.size(); i++){
+        
+        res.push_back(A[i]);
+      
+        if(umap.find(A[i]) == umap.end()){  //check whether that number previously occurred or not
+            umap[A[i]].push_back(i);
+        }
+        else{
+            int ind1 = INT_MAX, ind2;
+            
+            vector<int> v = umap[A[i]];
+            
+            for(int j = 0; j < v.size(); j++){  //number occurred, so taking minimum index of that number
+                if(ind1 > v[j]){
+                    ind1 = v[j];
+                    ind2 = j;
+                }
+            }
+            v.erase(v.begin()+ind2);  //deleting the minimum index from v
+            
+            v.push_back(i);   //inserting the new current index pf the number
+            
+            umap[A[i]+1].push_back(ind1); //inserting the minimum index
+            
+            res[ind1]++;    //incrementing the value at minimum index in res vector
+            
+            umap[A[i]] = v; //Assigning updated vector of indexes 
+        }
+    }
+    return res;
+}
