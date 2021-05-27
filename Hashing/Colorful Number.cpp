0c0f0c9ad346d36df9609d2ab05738604bd3d664
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-05-27 16:21:59 +0530

    Create Colorful Number.cpp

---
+++ Hashing/Colorful Number.cpp
@@ -0,0 +1,22 @@
+int Solution::colorful(int A) {
+    
+    string s = to_string(A);
+    
+    unordered_map<int,int> umap;
+    
+    for(int i = 0; i < s.size(); i++){
+        
+        int prod = 1;
+        
+        for(int j = i; j < s.size(); j++){
+          
+            prod *= s[j] - '0';
+         
+            if(umap.find(prod) != umap.end()){
+                return 0;
+            }
+            umap[prod]++;
+        }
+    }
+    return 1;
+}
