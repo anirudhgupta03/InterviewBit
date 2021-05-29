b3d64285f82b7e21a74c082e7771fe5f40d4b7bd
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-05-29 22:46:24 +0530

    Create Kth Smallest Element In Tree.cpp

---
+++ Tree Data Structure/Kth Smallest Element In Tree.cpp
@@ -0,0 +1,33 @@
+//Using Max Heap + Preorder Traversal
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+void solve(TreeNode* root, int B, priority_queue<int,vector<int>> &pq){
+    
+    if(root == NULL){
+        return;
+    }
+    
+    pq.push(root->val);
+    
+    if(pq.size() > B){
+        pq.pop();
+    }
+    
+    solve(root -> left,B,pq);
+    solve(root -> right,B,pq);
+}
+int Solution::kthsmallest(TreeNode* A, int B) {
+    
+    priority_queue<int> pq;
+    
+    solve(A,B,pq);
+    
+    return pq.top();
+}
