3dc8ded7a775c67584408c41a08d412c78ab608a
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-05-30 14:57:55 +0530

    Update Identical Binary Trees.cpp

--- Tree Data Structure/Identical Binary Trees.cpp
+++ Tree Data Structure/Identical Binary Trees.cpp
@@ -7,25 +7,18 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-void inorder(TreeNode* root, vector<int> &temp){
-    
-    if(root == NULL){
-        return;
-    }
-    
-    inorder(root->left,temp);
-    temp.push_back(root->val);
-    inorder(root->right,temp);
-}
 int Solution::isSameTree(TreeNode* A, TreeNode* B) {
     
-    vector<int> temp1,temp2;
-    
-    inorder(A,temp1);
-    inorder(B,temp2);
-    
-    if(temp1 == temp2){
+    if(A == NULL && B == NULL){
         return 1;
     }
-    return 0;
+    
+    if(A == NULL || B == NULL){
+        return 0;
+    }
+    
+    if(A -> val != B -> val){
+        return 0;
+    }
+    return isSameTree(A->left,B->left) && isSameTree(A->right,B->right);
 }
