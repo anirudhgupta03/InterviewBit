4bcc017db70787f5126b5efeb53795aa7c414bdf
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-05-31 10:42:32 +0530

    Create Merge two Binary Tree.cpp

---
+++ Tree Data Structure/Merge two Binary Tree.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+TreeNode* Solution::solve(TreeNode* A, TreeNode* B) {
+    
+    if(A != NULL && B == NULL){
+        return A;
+    }
+    
+    if(A == NULL &&  B != NULL){
+        return B;
+    }
+    
+    if(A != NULL && B != NULL){
+        A -> val = A -> val + B -> val;
+        A -> left = solve(A -> left,B -> left);
+        A -> right = solve(A -> right,B -> right);
+    }
+    return A;
+}
