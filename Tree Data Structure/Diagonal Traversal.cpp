f98ad5fa1720482cf4994bb01abf6e63a5326e33
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-05-31 13:46:53 +0530

    Create Diagonal Traversal.cpp

---
+++ Tree Data Structure/Diagonal Traversal.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+void diagonalTraverse(TreeNode* A, map<int,vector<int>> &mymap, int d){
+    
+    if(A == NULL){
+        return;
+    }
+    
+    mymap[d].push_back(A->val);
+    diagonalTraverse(A->left,mymap,d+1);
+    diagonalTraverse(A->right,mymap,d);
+}
+vector<int> Solution::solve(TreeNode* A) {
+    
+    vector<int> res;
+    if(A == NULL){
+        return res;
+    }
+    
+    map<int,vector<int>> mymap;
+    
+    diagonalTraverse(A,mymap,0);
+    
+    vector<int> temp;
+    for(auto x : mymap){
+        temp = x.second;
+        for(int i = 0; i < temp.size(); i++)
+            res.push_back(temp[i]);
+    }
+    return res;
+}
