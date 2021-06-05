d5a6b1a1339f6e0e4488c40e67d5abfb8b8b60f8
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-06-05 16:18:03 +0530

    Update Find Permutation - Optimal.cpp

--- Arrays/Find Permutation - Optimal.cpp
+++ Arrays/Find Permutation - Optimal.cpp
@@ -1,5 +1,6 @@
-//Gives lexicographically smallest number 
-
+//Method - 1
+//Gives lexicographically smallest number
+//Ref: https://www.youtube.com/watch?v=BPqE8FomgZ0
 vector<int> Solution::findPerm(const string A, int B) {
     
     int end = 0, start = 1;
@@ -19,5 +20,30 @@ vector<int> Solution::findPerm(const string A, int B) {
     for(int j = end; j >= start; j--){
                 ans.push_back(j);
     }
+    return ans;
+}
+//Method - 2
+//Optimal Approach
+//Time Complexity - O(N)
+//Space Complexity - O(1)
+vector<int> Solution::findPerm(const string A, int B) {
+    
+    int a = 1, b= B;
+    
+    vector<int> ans;
+    
+    for(int i = 0; i < A.size(); i++){
+        
+        if(A[i] == 'D'){
+            ans.push_back(b);
+            b--;
+        }
+        else{
+            ans.push_back(a);
+            a++;
+        }
+        
+    }
+    ans.push_back(a);
     return ans;
 }
