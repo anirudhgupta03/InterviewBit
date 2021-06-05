bfb5e522e2e13fc637eb80c2e618cde74c237aa3
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-06-05 10:52:43 +0530

    Math | Modulus

--- Arrays/Maximum Absolute Difference.cpp
+++ Arrays/Maximum Absolute Difference.cpp
@@ -1,3 +1,9 @@
+//Brute Force Approach
+//For every pair of (i,j) evaluate the expression and keep updating the ans.
+//Time Complexity - O(N^2)
+//Space Complexity - O(1)
+
+//Optimal Approach
 /*
 f(i, j) = |A[i] - A[j]| + |i - j| can be written in 4 ways 
 (Since we are looking at max value, we don’t even care if the value becomes negative as long as we are also covering the max value in some way).
@@ -11,7 +17,9 @@ Note that case 1 and 4 are equivalent and so are case 2 and 3.
 We can construct two arrays with values: A[i] + i and A[i] - i. 
 Then, for above 2 cases, we find the maximum value possible. For that, we just have to store minimum and maximum values of expressions A[i] + i and A[i] - i for all i.
 */
-
+//Ref: https://www.youtube.com/watch?v=Ov4weYCIipg
+//Time Complexity - O(N)
+//Space Complexity - O(N)
 int Solution::maxArr(vector<int> &A) {
     
     int n=A.size();
