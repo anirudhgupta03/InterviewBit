6231497c050c3f4c307ec36e0ad6d23a6ab12284
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-06-05 13:41:49 +0530

    Update Merge Overlapping Intervals.cpp

--- Arrays/Merge Overlapping Intervals.cpp
+++ Arrays/Merge Overlapping Intervals.cpp
@@ -18,8 +18,8 @@
 //Other than the sort invocation, we do a simple linear scan of the list, so the runtime is dominated by the O(nlogn) complexity of sorting.
 //Space complexity : O(1)
 
-If we can sort intervals in place, we do not need more than constant additional space, although the sorting itself takes O(\log n)O(logn) space. 
-Otherwise, we must allocate linear space to store a copy of intervals and sort that.
+// If we can sort intervals in place, we do not need more than constant additional space, although the sorting itself takes O(\log n)O(logn) space. 
+// Otherwise, we must allocate linear space to store a copy of intervals and sort that.
 
 bool cmp(Interval i1,Interval i2){
     return i1.start < i2.start;
