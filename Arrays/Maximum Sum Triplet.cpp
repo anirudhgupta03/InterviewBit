18f40d3c399938a0706c299721a40e09af88d568
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-06-05 09:48:57 +0530

    Update Maximum Sum Triplet.cpp

--- Arrays/Maximum Sum Triplet.cpp
+++ Arrays/Maximum Sum Triplet.cpp
@@ -1,4 +1,45 @@
+//Brute Force Approach
+//Simple Approach is to traverse for every triplet with three nested ‘for loops’ and find update 
+//the sum of all triplets one by one. Time complexity of this approach is O(n3) which is not sufficient for a larger value of ‘n’. 
+//Time Complexity - O(N^2)
+//Space Complexity - O(1)
+//Time Limit Exceeded
+
+//Better Approach
+//Time Complexity - O(N^2)
+//Space Complexity - O(N^2)
+//Time Limit Exceeded
+int Solution::solve(vector<int> &A) {
+    
+    int n = A.size();
+    
+    int ans = INT_MIN;
+    
+    for(int j = 1; j < n - 1; j++){
+        
+        int e1 = INT_MIN, e2 = INT_MIN;
+        
+        for(int i = 0; i < j; i++){
+            if(A[i] < A[j]){
+                e1 = max(A[i],e1);
+            }
+        }
+        
+        for(int k = j + 1; k < n; k++){
+            if(A[k] > A[j]){
+                e2 = max(A[k],e2);
+            }
+        }
+        
+        if(e1 != INT_MIN && e2 != INT_MIN){
+            ans = max(ans,e1+e2+A[j]);
+        }
+    }
+    return ans;
+}
+
 //Best Approach:
+//Ref: https://www.youtube.com/watch?v=l_hPdol4CSU
 //Best and efficient approach is use the concept of maximum suffix-array and binary search.
 
 //For finding maximum number greater than given number beyond it, we can maintain a maximum suffix-array array such that for any number(suffix[i]) it would 
@@ -28,9 +69,9 @@ int Solution::solve(vector<int> &A) {
     set<int> s;
     set<int> :: iterator it;
     int sum=0,ans=0;
-    for(int i=1;i<n-1;i++){
+    for(int i=1;i<n-1;i++){         //O(N)
         s.insert(A[i-1]);
-        it = s.lower_bound(A[i]);
+        it = s.lower_bound(A[i]);       //O(logN)
         it--;
         if(b[i]>A[i]&&((*it)<A[i])){
             sum = *it + A[i] +b[i];
