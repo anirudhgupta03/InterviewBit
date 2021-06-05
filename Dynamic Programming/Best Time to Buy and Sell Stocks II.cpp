e5411238307b5a221a07c7b5f3a5f5431fbfcce5
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-06-05 20:26:10 +0530

    Create Best Time to Buy and Sell Stocks II.cpp

---
+++ Dynamic Programming/Best Time to Buy and Sell Stocks II.cpp
@@ -0,0 +1,14 @@
+//Ref: https://www.youtube.com/watch?v=Q7v239y-Tik
+int Solution::maxProfit(const vector<int> &A) {
+    
+    int profit = 0;
+    
+    for(int i = 1; i < A.size(); i++){
+        
+        if(A[i] > A[i-1]){
+            profit += A[i] - A[i-1];
+        }
+    }
+    
+    return profit;
+}
