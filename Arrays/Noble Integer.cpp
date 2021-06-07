6a83cdee02de6611feec14e86b5c1f1454191501
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-06-07 11:58:52 +0530

    Sorting

--- Arrays/Noble Integer.cpp
+++ Arrays/Noble Integer.cpp
@@ -1,3 +1,5 @@
+//Time Complexity - O(NlogN)
+//Space Complexity - O(N)
 int Solution::solve(vector<int> &A) {
     
     sort(A.begin(),A.end());
@@ -29,5 +31,50 @@ int Solution::solve(vector<int> &A) {
         }
     }
     
+    return -1;
+}
+//Time Complexity - O(NlogN)
+//Space Complexity - O(N)
+//Using upper_bound function
+int Solution::solve(vector<int> &A) {
+    
+    sort(A.begin(),A.end());
+    
+    unordered_set<int> s;
+    
+    for(auto x: A){
+        s.insert(x);
+    }
+    
+    int count = 0;
+
+    for(auto x: s){
+        
+        int c = A.end() - upper_bound(A.begin(),A.end(),x);
+        if(c == x){
+            return 1;
+        }
+    }
+    return -1;
+}
+//Time Complexity - O(NlogN)
+//Space Complexity - O(1)
+int Solution::solve(vector<int> &A) {
+    
+    sort(A.begin(),A.end());
+    
+    if(A[A.size()-1] == 0){         //if last element is zero then no issue
+        return 1;
+    }
+    
+    for(int i = 0; i < A.size(); i++){
+        
+        if(A[i] != A[i+1]){
+            int count = A.size() - i - 1;
+            if(count == A[i]){
+                return 1;
+            }
+        }
+    }
     return -1;
 }
