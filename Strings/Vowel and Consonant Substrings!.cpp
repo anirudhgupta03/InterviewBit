03792852dc6c00d0aa9ce073af33ca1659276379
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-06-07 13:51:50 +0530

    Update Vowel and Consonant Substrings!.cpp

--- Strings/Vowel and Consonant Substrings!.cpp
+++ Strings/Vowel and Consonant Substrings!.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 int Solution::solve(string A) {
     
     long long i = 0, C = 0, V = 0, ans = 0;
@@ -22,4 +23,65 @@ int Solution::solve(string A) {
     }
     
     return ans%(1000000007);
+}
+//Method - 2
+//Time Complexity - O(N)
+//Space Complexity -O(1)
+#define ll long long
+int Solution::solve(string A) {
+    
+    int n = A.size();
+    
+    int mod = 1e9 + 7;
+    
+    ll count = 0,ans = 0;
+    
+    for(int i = n - 1; i >= 0; i--){
+        
+        if(A[i] == 'a' || A[i] == 'e' || A[i] == 'i' || A[i] == 'o' || A[i] == 'u'){
+            count++;
+        }
+        else{
+            ans += count;
+        }
+    }
+
+    count = 0;
+    
+    for(int i = n - 1; i >= 0; i--){
+        
+        if(A[i] == 'a' || A[i] == 'e' || A[i] == 'i' || A[i] == 'o' || A[i] == 'u'){
+            ans += count;
+        }
+        else{
+            count++;
+        }
+    }
+    
+    return ans%mod;
+}
+//Method - 3
+//Time Complexity - O(N)
+//Space Complexity -O(1)
+#define ll long long
+int Solution::solve(string A) {
+    
+    int n = A.size();
+    
+    int mod = 1e9 + 7;
+    
+    ll vow = 0, cons = 0,ans = 0;
+    
+    for(int i = n - 1; i >= 0; i--){
+        
+        if(A[i] == 'a' || A[i] == 'e' || A[i] == 'i' || A[i] == 'o' || A[i] == 'u'){
+            vow++;
+            ans += cons;
+        }
+        else{
+            cons++;
+            ans += vow;
+        }
+    }
+    return ans%mod;
 }
