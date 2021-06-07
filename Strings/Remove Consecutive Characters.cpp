cd87a047e590b9f3fe3a61337bf16ac77c532b08
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-06-07 14:05:02 +0530

    Update Remove Consecutive Characters.cpp

--- Strings/Remove Consecutive Characters.cpp
+++ Strings/Remove Consecutive Characters.cpp
@@ -1,3 +1,31 @@
+//Time Complexity - O(N^2)
+//Space Complexity - O(1)
+string Solution::solve(string A, int B) {
+    
+    int n = A.size();
+    
+    for(int i = 1; i < A.size(); i++){
+        
+        if(A[i] != A[i-1]){
+            A.insert(A.begin()+i,' ');     //Costly Operation insert(): Inserts additional characters into the string at a particular position. Its time complexity is O(N) where N is the size of the new string.
+            i++;
+        }
+    }
+    
+    int count = 0;
+    stringstream X(A);
+    string temp,res;
+    
+    while(X >> temp){
+        if(temp.size() != B){
+            res += temp;
+        }
+    }
+    return res;
+}
+//Time Complexity - O(N)
+//Space Complexity - O(1)
+//Optimal Approach
 string Solution::solve(string A, int B) {
     
     string ans;
@@ -22,3 +50,4 @@ string Solution::solve(string A, int B) {
     
     return ans;
 }
+
