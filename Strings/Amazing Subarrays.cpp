08826f8b81d3dc09978e5774659bb7f6be9af03e
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-06-08 16:50:27 +0530

    Create Amazing Subarrays.cpp

---
+++ Strings/Amazing Subarrays.cpp
@@ -0,0 +1,13 @@
+int Solution::solve(string A) {
+    
+    int count = 0;
+    int n = A.size();
+    
+    for(int i = 0; i < n; i++){
+        
+        if(A[i] == 'a' || A[i] == 'e' || A[i] == 'i' || A[i] == 'o' || A[i] == 'u' || A[i] == 'A' || A[i] == 'E' || A[i] == 'I' || A[i] == 'O' || A[i] == 'U'){
+            count += n - i;
+        }
+    }
+    return count%10003;
+}
