44424bb36b127f4e492a7dc7925cdcc59672c2c7
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-06-08 16:23:08 +0530

    Update Max Distance - Optimal.cpp

--- Arrays/Max Distance - Optimal.cpp
+++ Arrays/Max Distance - Optimal.cpp
@@ -1,3 +1,27 @@
+//Method - 1
+//Time Complexity - O(NlogN)
+//Space Complexity - O(N)
+int Solution::maximumGap(const vector<int> &A) {
+    
+    vector<pair<int,int>> v;
+    
+    for(int i = 0; i < A.size(); i++){
+        v.push_back(make_pair(A[i],i));
+    }
+    
+    sort(v.begin(),v.end());
+    
+    int i = v[v.size()-1].second ,ans = 0;
+    for(int j = v.size()-2; j >= 0; j--){
+        ans = max(ans,i - v[j].second);
+        i = max(v[j].second,i);
+    }
+    return ans;
+}
+
+//Method - 2
+//Time Complexity - O(N)
+//Space Complexity - O(N)
 int Solution::maximumGap(const vector<int> &A) {
     
     int n = A.size();
