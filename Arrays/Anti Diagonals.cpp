0b2768da03627e016862e0339ccee2ab693a5bc2
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-06-10 13:17:06 +0530

    Update Anti Diagonals.cpp

--- Arrays/Anti Diagonals.cpp
+++ Arrays/Anti Diagonals.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 vector<vector<int> > Solution::diagonal(vector<vector<int> > &A) {
     
     vector<vector<int>> v1;
@@ -38,4 +39,48 @@ vector<vector<int> > Solution::diagonal(vector<vector<int> > &A) {
     }
     
     return v1;
+}
+
+//Method - 2
+vector<vector<int> > Solution::diagonal(vector<vector<int> > &A) {
+    
+    int n = A.size();
+    
+    vector<vector<int>> res;
+    
+    int j = 0;
+    
+    while(j < n){
+        
+        vector<int> temp;
+        
+        int p = 0;
+        
+        for(int k = j; k >= 0; k--){
+            temp.push_back(A[p][k]);
+            p++;
+        }
+        
+        res.push_back(temp);
+        j++;
+    }
+    
+    int i = 1;
+    
+    while(i < n){
+        
+        vector<int> temp;
+        
+        int p = i;
+        
+        for(int k =  n - 1; k >= i; k--){
+            temp.push_back(A[p][k]);
+            p++;
+        }
+        
+        res.push_back(temp);
+        i++;
+    }
+    
+    return res;
 }
