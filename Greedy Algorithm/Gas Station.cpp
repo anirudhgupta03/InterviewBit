aaaddc18b5e17b5ad4d733629a3fd3173307c54c
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-06-10 10:08:33 +0530

    Update Gas Station.cpp

--- Greedy Algorithm/Gas Station.cpp
+++ Greedy Algorithm/Gas Station.cpp
@@ -1,3 +1,12 @@
+//Brute Force Approach
+//Start from every i, and check to see if every point is reachable with the gas available. Return the first i for which you can complete the trip without the gas reaching a negative number.
+//This approach would however be quadratic.
+//Time Complexity - O(N^2)
+//Space Complexity - O(1)
+
+//Ref: https://www.youtube.com/watch?v=nTKdYm_5-ZY
+//Time Complexity - O(N)
+//Space Complexity - O(1)
 int Solution::canCompleteCircuit(const vector<int> &A, const vector<int> &B) {
     
     int s = 0, d = 0, start = 0, n = A.size();
