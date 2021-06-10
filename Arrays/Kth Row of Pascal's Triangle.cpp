385e41bb2a889f0cd66ec350afb987c1ac9c0888
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-06-10 11:25:33 +0530

    Update Kth Row of Pascal's Triangle.cpp

--- Arrays/Kth Row of Pascal's Triangle.cpp
+++ Arrays/Kth Row of Pascal's Triangle.cpp
@@ -1,3 +1,6 @@
+//Brute Force Approach
+//Time Complexity - O(A^2)
+//Space Complexity - O(A^2)
 vector<int> Solution::getRow(int A) {
     
     vector<vector<int>> v1;
@@ -23,4 +26,41 @@ vector<int> Solution::getRow(int A) {
     }
     
     return v1[A];
+}
+
+//Method - 2
+//Time Complexity - O(A^2)
+//Space Complexity - O(A)
+vector<int> Solution::getRow(int A) {
+    
+    vector<int> res(A+1);
+    
+    for(int i = 0; i <= A; i++){
+        
+        for(int j = i; j >= 0; j--){
+            
+            if(j == 0 || j == i){
+                res[j] = 1;
+            }
+            else{
+                res[j] = res[j] + res[j-1];
+            }
+        }
+    }
+    return res;
+}
+
+//Method - 3
+//Time Complexity - O(A)
+//Space Complexity - O(A)
+vector<int> Solution::getRow(int A) {
+    
+    vector<int> res(A+1);
+    res[0] = 1;
+    
+    for(int i = 1; i < A + 1; i++){
+        res[i] = res[i-1]*(A-i+1);
+        res[i] /= i;
+    }
+    return res;
 }
