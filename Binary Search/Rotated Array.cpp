3c7899947e528dcb4f08850852cff866465fb3f9
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-06-10 16:48:39 +0530

    Update Rotated Array.cpp

--- Binary Search/Rotated Array.cpp
+++ Binary Search/Rotated Array.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 int Solution::findMin(const vector<int> &A) {
     
     int n = A.size();
@@ -20,6 +21,66 @@ int Solution::findMin(const vector<int> &A) {
         }
         else if(A[mid] <= A[r]){
             r = mid -1;
+        }
+    }
+}
+
+//Method - 2
+int Solution::findMin(const vector<int> &A) {
+    
+    int n = A.size();
+    
+    if(n == 1){
+        return A[0];
+    }
+    int lo = 0, hi = n - 1;
+    
+    while(lo <= hi){
+        
+        int mid = (lo + hi)/2;
+        int prev = (mid+n-1)%n;
+        int next = (mid + n - 1)%n;
+        
+        if(A[mid] < A[prev] && A[mid] < A[next]){
+            return A[mid];
+        }
+        else if(A[mid] < A[n-1]){
+            hi = mid - 1;
+        }
+        else{
+            lo = mid + 1;
+        }
+    }
+}
+
+//Method - 3
+int Solution::findMin(const vector<int> &A) {
+    
+    int n = A.size();
+    
+    if(n == 1){
+        return A[0];
+    }
+    int lo = 0, hi = n - 1;
+    
+    while(lo <= hi){
+        
+        int mid = (lo + hi)/2;
+        
+        if(mid == 0 && A[0] <= A[1] && A[0] <= A[n-1]){
+            return A[mid];
+        }
+        else if(mid == n - 1 && A[n-1] <= A[n-2] && A[n-1] <= A[0]){
+            return A[mid];
+        }
+        else if(A[mid] <= A[mid-1] && A[mid] <= A[mid+1]){
+            return A[mid];
+        }
+        else if(A[mid] <= A[hi]){
+            hi = mid - 1;
+        }
+        else if(A[mid] >= A[lo]){
+            lo = mid + 1;
         }
     }
 }
