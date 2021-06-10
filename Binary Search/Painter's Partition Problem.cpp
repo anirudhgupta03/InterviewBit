5f3ea1f3e861346aa142671e137b8aa7f2d73d3c
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-06-10 18:30:02 +0530

    Update Painter's Partition Problem.cpp

--- Binary Search/Painter's Partition Problem.cpp
+++ Binary Search/Painter's Partition Problem.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 long long number_of_painters(vector<int> C, int t){
     
     long long count = 1, sum = 0;
@@ -36,4 +37,57 @@ int Solution::paint(int A, int B, vector<int> &C) {
         }
     }
     return (ans*B)%10000003;
+}
+
+//Method - 2
+#define ll long long
+bool isPossible(int t, vector<int> &C, int A){
+    
+    ll count = 1;
+    
+    ll sum = 0;
+    
+    for(int i = 0; i < C.size(); i++){
+        
+        sum += C[i];
+        
+        if(sum > t){
+            sum = C[i];
+            count++;
+        }
+    }
+    
+    if(count > A){
+        return false;
+    }
+    return true;
+}
+int Solution::paint(int A, int B, vector<int> &C) {
+    
+    int mod = 10000003;
+    
+    if(A >= C.size()){
+        return (*max_element(C.begin(),C.end())*B)%mod;
+    }
+    
+    ll lo = INT_MIN, hi = 0;
+    
+    for(int i = 0; i < C.size(); i++){
+        if(C[i] > lo) lo = C[i];
+        hi += C[i];
+    }
+    ll ans;
+    while(lo <= hi){
+        
+        ll mid = (lo + hi)/2;
+        
+        if(isPossible(mid,C,A)){
+            ans = mid;
+            hi = mid -1;
+        }
+        else{
+            lo = mid + 1;
+        }
+    }
+    return (ans*B)%mod;
 }
