a178b7f084c3bc4a5ad56d6f716480c06b91a073
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-06-10 17:03:45 +0530

    Update WoodCutting Made Easy!.cpp

--- Binary Search/WoodCutting Made Easy!.cpp
+++ Binary Search/WoodCutting Made Easy!.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 long long woodcut(int height, vector<int> A){
     
     long long wood = 0;
@@ -32,4 +33,47 @@ int Solution::solve(vector<int> &A, int B) {
         }
     }
     return ans;
+}
+
+//Method - 2
+#define ll long long
+bool isPossibleToCut(int h, vector<int> &A, int B){
+    
+    ll woodCut = 0;
+    
+    for(int i = 0; i < A.size(); i++){
+        
+        if(A[i]-h > 0){
+            woodCut += A[i]-h;
+        }
+    }
+    
+    if(woodCut >= B){
+        return true;
+    }
+    return false;
+}
+int Solution::solve(vector<int> &A, int B) {
+    
+    int lo = 0, hi = INT_MIN;
+    
+    for(int x: A){
+        hi = max(hi,x);
+    }
+    
+    int reqHeight;
+    
+    while(lo <= hi){
+        
+        int mid = (lo + hi)/2;
+        
+        if(isPossibleToCut(mid,A,B)){
+            reqHeight = mid;
+            lo = mid + 1;
+        }
+        else{
+            hi = mid - 1;
+        }
+    }
+    return reqHeight;
 }
