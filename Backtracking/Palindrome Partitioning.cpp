a56abeb723f823d6fdbca8c08069f85e10e27099
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-06-11 17:47:28 +0530

    Update Palindrome Partitioning.cpp

--- Backtracking/Palindrome Partitioning.cpp
+++ Backtracking/Palindrome Partitioning.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 bool ispalindrome(string s){
     for(int i=0;i<s.length()/2;i++){
         if(s[i]!=s[s.length()-i-1])return false;
@@ -31,4 +32,45 @@ vector<vector<string> > Solution::partition(string A) {
     solve(0,A,p,v);
     
     return v;
+}
+
+//Method - 2
+bool isPalindrome(string s){
+    
+    int l = 0, r = s.size() - 1;
+    
+    while(l <= r){
+        if(s[l] != s[r]){
+            return false;
+        }
+        l++;
+        r--;
+    }
+    return true;
+}
+void solve(int ind, string A, vector<string> &temp, vector<vector<string>> &res){
+    
+    if(ind == A.size()){
+        res.push_back(temp);
+        return;
+    }
+    for(int i = ind; i < A.size(); i++){
+        
+        string p = A.substr(ind,i-ind+1);
+        
+        if(isPalindrome(p)){
+            temp.push_back(p);
+            solve(i+1,A,temp, res);
+            temp.pop_back();
+        }
+    }
+}
+vector<vector<string> > Solution::partition(string A) {
+    
+    vector<string> temp;
+    vector<vector<string>> res;
+    
+    solve(0,A,temp,res);
+    
+    return res;
 }
