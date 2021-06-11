3deb17ba1857ea208fafd89e43d5ef5ac12394c2
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-06-11 22:37:52 +0530

    Update Spiral Order Matrix I.cpp

--- Arrays/Spiral Order Matrix I.cpp
+++ Arrays/Spiral Order Matrix I.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 vector<int> Solution::spiralOrder(const vector<vector<int> > &A) {
     
     int rows = A.size(),columns = A[0].size();
@@ -53,4 +54,72 @@ vector<int> Solution::spiralOrder(const vector<vector<int> > &A) {
     }
     
     return v;
+}
+
+//Method - 2
+vector<int> Solution::spiralOrder(const vector<vector<int> > &A) {
+    
+    int m = A.size(), n = A[0].size();
+    
+    vector<int> res;
+    
+    int t = 0, r = n - 1, l = 0, b = m - 1;
+    
+    int flag = 0;
+    
+    int i = 0, j = 0;
+    
+    int count = 0;
+    
+    while(count <= m*n){
+        
+        if(count == m*n){
+            break;
+        }
+        if(flag == 0){
+            
+            while(j <= r){
+                res.push_back(A[i][j]);
+                count++;
+                j++;
+            }
+            j--;
+            i++;
+            
+            t++;
+            r--;
+            
+            while(i <= b){
+                res.push_back(A[i][j]);
+                count++;
+                i++;
+            }
+            i--;
+            j--;
+            flag = 1;
+            b--;
+        }
+        else{
+            
+            while(j >= l){
+                res.push_back(A[i][j]);
+                count++;
+                j--;
+            }
+            j++;
+            i--;
+            
+            l++;
+            while(i >= t){
+                res.push_back(A[i][j]);
+                count++;
+                i--;
+            }
+            i++;
+            j++;
+            flag = 0;
+        }
+    }
+    
+    return res;
 }
