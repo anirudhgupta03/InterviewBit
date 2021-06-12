d2eb703bca0d21d5b95bfa75351d3825ffd7248c
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-06-12 10:35:08 +0530

    Math

--- Arrays/Large Factorial.cpp
+++ Arrays/Large Factorial.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 string Solution::solve(int A) {
     
     int fact[201]={0};
@@ -36,4 +37,35 @@ string Solution::solve(int A) {
     reverse(s.begin(),s.end());
     
     return s;
+}
+
+//Method - 2
+//Ref: https://www.youtube.com/watch?v=2fYZERB2Yng
+string Solution::solve(int A) {
+    
+    vector<int> fact;
+    fact.push_back(1);
+    
+    for(int i = 2; i <= A; i++){
+        
+        int carry = 0;
+        
+        for(int j = 0; j < fact.size(); j++){
+            int t = fact[j]*i + carry;
+            fact[j] = t%10;
+            carry = t/10;
+        }
+        
+        while(carry != 0){
+            fact.push_back(carry%10);
+            carry = carry/10;
+        }
+        
+    }
+    
+    string res;
+    for(int i = fact.size() - 1; i >= 0; i--){
+        res.push_back(fact[i] + '0');
+    }
+    return res;
 }
