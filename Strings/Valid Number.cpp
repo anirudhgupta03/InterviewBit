6f9f0de5f6689f0002e8253df34349712db1a3ae
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-06-12 13:34:47 +0530

    Update Valid Number.cpp

--- Strings/Valid Number.cpp
+++ Strings/Valid Number.cpp
@@ -1,3 +1,79 @@
+//Brute Force Approach
+//Time Complexity - O(N)
+//Space Complexity - O(1)
+bool isDigit(char c){
+    
+    if(c >= '0' && c <= '9'){
+        return true;
+    }
+    return false;
+}
+int Solution::isNumber(const string A) {
+    
+    int n = A.size();
+    
+    bool flag1 = false; //operator '+' '-'
+    bool flag2 = false; //dot
+    bool flag3 = false; //num
+    
+    for(int i = 0; i < n; i++){
+        
+        if(isalpha(A[i]) && A[i] != 'e'){
+            return 0;
+        }
+        else if(A[i] == 'e'){
+            
+            if(i == 0 || i == n - 1 || !isDigit(A[i-1])){
+                return 0;
+            }
+            
+            int pos = i;
+	    i++;
+            
+            while(i < n){
+                
+                if(A[i] == '.'){		//Dot not allowed after 'e'
+                    return 0;
+                }
+                else if(isalpha(A[i])){		//No characters allowed after 'e'
+                    return 0;
+                }
+                else if(A[i] == '+' || A[i] == '-'){	//If there is a '+' or '-' operator it should be next to 'e' and also after that a digit should be there
+                 
+                    if(i != pos + 1 || i == n - 1 || !isDigit(A[i+1])){
+                        return 0;
+                    }
+                }
+                i++;
+            }
+        }
+        else if(A[i] >= '0' && A[i] <= '9'){	
+            flag3 = true;
+        }
+        else if(A[i] == '+' || A[i] == '-'){
+            if(flag3 || i == n - 1 || !isDigit(A[i+1])){
+                return 0;
+            }
+            flag1 = true;
+        }
+        else if(A[i] == '.'){
+            if(flag2 || i == n - 1 || !isDigit(A[i+1])){
+                return 0;
+            }
+            flag2 = true;
+        }
+        else if(A[i] != ' '){
+            return 0;
+        }
+    }
+    return flag3;
+}
+
+//Optimal Approach
+//Time Complexity - O(N)
+//Space Complexity - O(1)
+//Setting a variable true means it cannot come further now
+//Setting a variable false means it can come further
 int Solution::isNumber(const string A) {
     
     int len = A.length();
@@ -10,7 +86,7 @@ int Solution::isNumber(const string A) {
     int l = 0, r = A.length() - 1;
     
     while(l <=r && A[l] == ' ')l++;
-	while(r >= 0 && A[r] == ' ')r--;
+    while(r >= 0 && A[r] == ' ')r--;
 
     while(l <= r){
         
@@ -23,7 +99,7 @@ int Solution::isNumber(const string A) {
             if(!num || e){
                 return false;
             }
-            e = true;
+            e = true;	
             num = false;
             sign = false; 
         }
