49133e42b455fa928e514891083393468b102041
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-06-17 10:08:29 +0530

    Update Reorder List.cpp

--- Linked List/Reorder List.cpp
+++ Linked List/Reorder List.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -57,5 +58,62 @@ ListNode* Solution::reorderList(ListNode* A) {
         
     }
     ptr3 -> next = ptr2;
+    return A;
+}
+
+//Method - 2
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ListNode* Solution::reorderList(ListNode* A) {
+    
+    if(A == NULL || A -> next == NULL || A -> next -> next == NULL){
+        return A;
+    }
+    
+    int len = 0;
+    ListNode* temp = A;
+    
+    while(temp){
+        temp = temp -> next;
+        len++;
+    }
+    
+    int pos = (len + 1)/2;
+    
+    temp = A;
+    
+    while(pos > 1){
+        temp = temp -> next;
+        pos--;
+    }
+    
+    ListNode* temp1 = NULL, *temp2 = temp ->next, *temp3 = temp -> next -> next;
+    temp -> next = NULL;
+    
+    while(temp3){
+        temp2 -> next = temp1;
+        temp1 = temp2;
+        temp2 = temp3;
+        temp3 = temp3 -> next;
+    }
+    temp2 -> next = temp1;
+    
+    ListNode* B = temp2;
+    temp1 = A, temp2 = B;
+ 
+    while(temp2){
+        temp = temp2;
+        temp2 = temp2 -> next;
+        
+        temp -> next = temp1 -> next;
+        temp1 -> next = temp;
+        temp1 = temp1 -> next -> next;
+    }
     return A;
 }
