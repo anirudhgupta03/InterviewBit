1d3d5743390cee754ad0f5bbc0f1d00a083d61a4
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-06-17 09:41:34 +0530

    Update Reverse Alternate K Nodes.cpp

--- Linked List/Reverse Alternate K Nodes.cpp
+++ Linked List/Reverse Alternate K Nodes.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -51,6 +52,71 @@ ListNode* Solution::solve(ListNode* A, int B) {
             temp1 = NULL;
             if(temp2 != NULL) temp3 = temp2 -> next;
             p++;
+        }
+    }
+    return A;
+}
+
+//Method - 2
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ListNode* Solution::solve(ListNode* A, int B) {
+    
+    if(A == NULL || A -> next == NULL || B == 1){
+        return A;
+    }
+    ListNode* ptr1 = NULL, *ptr2 = A, *ptr3 = A -> next;
+    
+    ListNode* prev = NULL, *start = A;
+    
+    int count = 0;
+    
+    bool flag = 1;
+    
+    while(ptr3){
+        
+        if(flag){
+            count = 0;
+            while(count != B - 1){
+                ptr2 -> next = ptr1;
+                ptr1 = ptr2;
+                ptr2 = ptr3;
+                ptr3 = ptr3 -> next;
+                count++;
+            }
+            ptr2 -> next = ptr1;
+            
+            if(prev == NULL) A = ptr2;
+            else prev -> next = ptr2;
+            
+            start -> next = ptr3;
+            flag = 0;
+        }
+        else{
+            
+            count = 0;
+            while(count != B - 1){
+                ptr3 = ptr3 -> next;
+                count++;
+            }
+            prev = ptr3;
+            
+            if(ptr3 -> next != NULL){
+                ptr1 = NULL;
+                ptr2 = ptr3 -> next;
+                ptr3 = ptr2 -> next;
+            }
+            else{
+                break;
+            }
+            start = ptr2;
+            flag = 1;
         }
     }
     return A;
