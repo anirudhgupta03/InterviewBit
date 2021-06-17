5a35c6edfb157781fa94fad166b66999b261f74a
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-06-17 11:58:25 +0530

    Update K reverse linked list.cpp

--- Linked List/K reverse linked list.cpp
+++ Linked List/K reverse linked list.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -43,6 +44,58 @@ ListNode* Solution::reverseList(ListNode* A, int B) {
         temp4 -> next = temp2;
         temp4 = temp5;
         temp5 = temp3;
+    }
+    return A;
+}
+
+//Method - 2
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ListNode* Solution::reverseList(ListNode* A, int B) {
+    
+    if(A == NULL || A -> next == NULL || B == 1){
+        return A;
+    }
+    ListNode* start = NULL, *end = NULL;
+    
+    ListNode* ptr1 = NULL, *ptr2 = A, *ptr3 = A -> next;
+    
+    ListNode* begin;
+    
+    int count = B;
+    
+    while(ptr3){
+        count = B;
+        begin = ptr2;
+        while(count != 1){
+            ptr2 -> next = ptr1;
+            ptr1 = ptr2;
+            ptr2 = ptr3;
+            ptr3 = ptr3 -> next;
+            count--;
+        }
+        ptr2 -> next = ptr1;
+        end = ptr2;
+        
+        if(start == NULL){
+            A = end;
+        }
+        else{
+            start -> next = end;
+        }
+        start = begin;
+        ptr1 = NULL;
+        ptr2 = ptr3;
+        
+        if(ptr3 != NULL){
+            ptr3 = ptr3 -> next;
+        }
     }
     return A;
 }
