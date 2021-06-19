a708badab767d0340e2d761baddc1cab54391a34
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-06-19 11:36:47 +0530

    Update Diffk.cpp

--- Two Pointers/Diffk.cpp
+++ Two Pointers/Diffk.cpp
@@ -1,3 +1,28 @@
+//Similar Problem: https://www.interviewbit.com/problems/pair-with-given-difference/
+//Method - 1
+//Time Complexity - O(NlogN)
+//Space Complexity - O(1)
+int Solution::diffPossible(vector<int> &A, int B) {
+    
+    for(int i = 0; i < A.size(); i++){
+        
+        int count = upper_bound(A.begin(),A.end(),A[i]-B) - lower_bound(A.begin(),A.end(),A[i]-B);
+        
+        if(B == 0){
+            if(count >= 2){
+                return true;
+            }
+        }
+        else if(count != 0){
+            return true;
+        }
+    }
+    return false;
+}
+
+//Method - 2
+//Time Complexity - O(N)
+//Space Complexity - O(1)
 int Solution::diffPossible(vector<int> &A, int B) {
     
     int l = 0, r = 1;
