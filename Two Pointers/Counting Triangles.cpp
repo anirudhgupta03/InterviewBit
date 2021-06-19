00dcab82b372a24392985ffc6dee25c372070120
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-06-19 11:06:07 +0530

    Update Counting Triangles.cpp

--- Two Pointers/Counting Triangles.cpp
+++ Two Pointers/Counting Triangles.cpp
@@ -1,6 +1,81 @@
+//Method - 1
+//TLE
+//Time Complexity - O(N^3)
+//Space Complexity - O(1)
+#define ll long long
 int Solution::nTriang(vector<int> &A) {
     
-    int mod = 1000000007;
+    int mod = 1e9 + 7;
+    
+    int n = A.size();
+    
+    if(n < 3){
+        return 0;
+    }
+    ll count = 0;
+    
+    sort(A.begin(),A.end());
+    
+    for(int i = 0; i <= A.size() - 3; i++){
+        
+        for(int j = i + 1; j <= A.size() - 2; j++){
+            
+            int s = A[i] + A[j];
+            
+            int k = j + 1;
+            
+            while(k < A.size()){
+                
+                if(A[i] + A[j] > A[k]){
+                    count++;
+                }
+                else{
+                    break;
+                }
+                
+                k++;
+            }
+        }
+    }
+    return count%mod;
+}
+
+//Method - 2
+//Time Complexity - O(N^2logN)
+//Space Complexity - O(1)
+#define ll long long
+int Solution::nTriang(vector<int> &A) {
+    
+    int mod = 1e9 + 7;
+    
+    int n = A.size();
+    
+    if(n < 3){
+        return 0;
+    }
+    ll count = 0;
+    
+    sort(A.begin(),A.end());
+    
+    for(int i = 0; i <= A.size() - 3; i++){
+        
+        for(int j = i + 1; j <= A.size() - 2; j++){
+            
+            int s = A[i] + A[j];
+            
+            count += lower_bound(A.begin()+j+1,A.end(),s) - (A.begin() + j + 1);
+        }
+    }
+    return count%mod;
+}
+
+//Method - 3
+//Optimal Approach
+//Time Complexity - O(N^2)
+//Space Complexity - O(1)
+int Solution::nTriang(vector<int> &A) {
+    
+    int mod = 1e9 + 7;
     
     sort(A.begin(),A.end());
     
