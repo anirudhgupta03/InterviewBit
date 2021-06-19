f1b8db2b14d79324bb19a0c41e576837d40d3cb2
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-06-19 13:03:39 +0530

    Create Maximum Ones After Modification.cpp

---
+++ Two Pointers/Maximum Ones After Modification.cpp
@@ -0,0 +1,27 @@
+int Solution::solve(vector<int> &A, int B) {
+    
+    int n = A.size();
+    
+    int count = 0;
+    
+    int lo = 0, hi = 0, maxLen = 0;
+    
+    while(hi < A.size()){
+        
+        if(A[hi] == 0){
+            count++;
+        }
+        
+        if(count > B){
+            maxLen = max(maxLen,hi-lo);
+            
+            while(A[lo] != 0 && lo < hi){
+                lo++;
+            }
+            lo++;
+            count--;
+        }
+        hi++;
+    }
+    return maxLen;
+}
