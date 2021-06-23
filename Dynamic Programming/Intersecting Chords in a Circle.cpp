c967b08bf99ce3528089fba0ef734aa456378a55
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-06-23 12:07:12 +0530

    Create Intersecting Chords in a Circle.cpp

---
+++ Dynamic Programming/Intersecting Chords in a Circle.cpp
@@ -0,0 +1,29 @@
+//Catalan Number
+//Ref: https://www.youtube.com/watch?v=qgQg1BcCWBA
+#define ll long long
+int Solution::chordCnt(int A) {
+    
+    if(A == 1 || A == 2){
+        return A;
+    }
+    
+    int mod = 1e9 + 7;
+    
+    vector<ll> dp(A+1,0);
+    
+    dp[0] = 1;
+    dp[1] = 1;
+    dp[2] = 2;
+    
+    for(int i = 3; i <= A; i++){
+        
+        int l = 0, r = i - 1;
+        
+        while(l <= i - 1){
+            dp[i] = (dp[i] + dp[l]*dp[r])%mod;
+            l++;
+            r--;
+        }
+    }
+    return dp[A]%mod;
+}
