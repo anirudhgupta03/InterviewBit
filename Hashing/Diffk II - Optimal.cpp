5bba7e889d3b0aa4d54d3843b3fd7f8c92c91933
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-06-26 17:26:30 +0530

    Update Diffk II - Optimal.cpp

--- Hashing/Diffk II - Optimal.cpp
+++ Hashing/Diffk II - Optimal.cpp
@@ -2,8 +2,6 @@ int Solution::diffPossible(const vector<int> &A, int B) {
     
     vector<int> temp = A;
     
-    //sort(temp.begin(),temp.end());
-    
     unordered_map<int,int> umap;
     
     for(int i = 0; i < temp.size(); i++){
@@ -11,9 +9,7 @@ int Solution::diffPossible(const vector<int> &A, int B) {
         if(umap.find(temp[i]-B) != umap.end() || umap.find(temp[i]+B) != umap.end()){
             return 1;
         }
-        else{
-            umap[temp[i]] = 1;
-        }
+        umap[temp[i]] = 1;
     }
     return 0;
 }
