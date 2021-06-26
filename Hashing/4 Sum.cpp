fd42d454a300d48471fefbe61978dd5670983fbd
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-06-26 17:13:37 +0530

    Update 4 Sum.cpp

--- Hashing/4 Sum.cpp
+++ Hashing/4 Sum.cpp
@@ -1,3 +1,6 @@
+//Method - 1
+//Time Complexity - O(N^3logN)
+//Space Complexity - O(N)
 vector<vector<int> > Solution::fourSum(vector<int> &A, int B) {
     
     set<vector<int>> s;
@@ -52,6 +55,57 @@ vector<vector<int> > Solution::fourSum(vector<int> &A, int B) {
     
     for(it = s.begin(); it != s.end(); it++){
         res.push_back(*it);
+    }
+    return res;
+}
+
+//Method - 2
+//Optimal Approach
+//Time Complexity - O(N^3)
+//Space Complexity - O(1)
+vector<vector<int> > Solution::fourSum(vector<int> &A, int B) {
+    
+    sort(A.begin(),A.end());
+
+    vector<vector<int>> res;
+    
+    int n = A.size();
+    
+    if(n < 4){
+        return res;
+    }
+    
+    for(int i = 0; i <= n - 3; i++){
+        if(i > 0 && A[i] == A[i-1]){
+            continue;
+        }
+        for(int j = i + 1; j <= n - 2; j++){
+            
+            if(j > i + 1 && A[j] == A[j-1]){
+                continue;
+            }
+            int lo = j + 1, hi = n - 1;
+            
+            while(lo < hi){
+                
+                int sum = A[i] + A[j] + A[lo] + A[hi];
+                
+                if(sum < B){
+                    lo++;
+                }
+                else if(sum > B){
+                    hi--;
+                }
+                else{
+                    vector<int> temp = {A[i],A[j],A[lo],A[hi]};
+                    res.push_back(temp);
+                    while(lo < hi && A[lo] == A[lo+1]) lo++;
+                    lo++;
+                    while(lo < hi && A[hi] == A[hi-1]) hi--;
+                    hi--;
+                }
+            }
+        }
     }
     return res;
 }
