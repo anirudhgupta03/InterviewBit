bed1ac9bb496a93201a2f075d5bc5cb880c647ce
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-06-26 17:33:44 +0530

    Update Pairs With Given Xor.cpp

--- Hashing/Pairs With Given Xor.cpp
+++ Hashing/Pairs With Given Xor.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 int Solution::solve(vector<int> &A, int B) {
     
     unordered_map<int,int> umap;
@@ -15,4 +16,22 @@ int Solution::solve(vector<int> &A, int B) {
         }
     }
     return count/2;
+}
+
+//Method - 2
+int Solution::solve(vector<int> &A, int B) {
+    
+    unordered_map<int,int> umap;
+    
+    int count = 0;
+    
+    for(int i = 0; i < A.size(); i++){
+        
+        if(umap.find(A[i]^B) != umap.end()){
+            count++;
+        }
+        umap[A[i]]++;
+    }
+    
+    return count;
 }
