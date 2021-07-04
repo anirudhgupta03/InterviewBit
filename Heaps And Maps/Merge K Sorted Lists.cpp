272657eeda92822f774d61bb9c7a2af3b1caa2e9
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-07-04 15:33:21 +0530

    Update Merge K Sorted Lists.cpp

--- Heaps And Maps/Merge K Sorted Lists.cpp
+++ Heaps And Maps/Merge K Sorted Lists.cpp
@@ -1,3 +1,71 @@
+//Method - 1
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ListNode* merge(ListNode* l1, ListNode* l2){
+    
+    if(l1 == NULL && l2 == NULL){
+        return NULL;
+    }
+    if(l1 == NULL){
+        return l2;
+    }
+    if(l2 == NULL){
+        return l1;
+    }
+    
+    ListNode* prev = NULL, *head;
+    
+    while(l1 && l2){
+        
+        if(l1 -> val < l2 -> val){
+            if(prev == NULL){
+                head = l1;
+            }
+            else{
+                prev -> next = l1;
+            }
+            prev = l1;
+            l1 = l1 -> next;
+        }
+        else{
+            if(prev == NULL){
+                head = l2;
+            }
+            else{
+                prev -> next = l2;
+            }
+            prev = l2;
+            l2 = l2 -> next;
+        }
+    }
+    
+    if(l1){
+        prev -> next = l1;
+    }
+    if(l2){
+        prev -> next = l2;
+    }
+    
+    return head;
+}
+ListNode* Solution::mergeKLists(vector<ListNode*> &A) {
+    
+    ListNode* head = A[0];
+    
+    for(int i = 1; i < A.size(); i++){
+        head = merge(head,A[i]);
+    }
+    return head;
+}
+
+//Method - 2
+//Optimal
 /**
  * Definition for singly-linked list.
  * struct ListNode {
