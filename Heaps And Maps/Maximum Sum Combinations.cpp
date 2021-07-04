4efb7221ab1c8676948d231699edaedd0aaaa627
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-07-04 15:21:00 +0530

    Update Maximum Sum Combinations.cpp

--- Heaps And Maps/Maximum Sum Combinations.cpp
+++ Heaps And Maps/Maximum Sum Combinations.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 vector<int> Solution::solve(vector<int> &A, vector<int> &B, int C) {
     
     sort(A.begin(),A.end(),greater<int>());
@@ -38,4 +39,42 @@ vector<int> Solution::solve(vector<int> &A, vector<int> &B, int C) {
     }
     sort(res.begin(),res.end(),greater<int>());
     return res;
+}
+
+//Method - 2
+vector<int> Solution::solve(vector<int> &A, vector<int> &B, int C) {
+    
+    sort(A.begin(),A.end(),greater<int>());
+    sort(B.begin(),B.end(),greater<int>());
+    
+    priority_queue<int,vector<int>,greater<int>> pq;
+    
+    for(int i = 0; i < A.size(); i++){
+        for(int j = 0; j < B.size(); j++){
+            
+            int sum = A[i] + B[j];
+            
+            if(pq.size() < C){
+                pq.push(sum);
+            }
+            else{
+                
+                if(pq.top() < sum){
+                    pq.pop();
+                    pq.push(sum);
+                }
+                else{
+                    break;
+                }
+            }
+        }
+    }
+    
+    vector<int> res;
+    while(pq.size()){
+        res.push_back(pq.top());
+        pq.pop();
+    }
+    reverse(res.begin(),res.end());
+    return  res;
 }
