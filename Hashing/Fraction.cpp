68a208ef9e0c4e259e33f8865d18dc6162d33416
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-07-04 11:48:29 +0530

    Update Fraction.cpp

--- Hashing/Fraction.cpp
+++ Hashing/Fraction.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 #define ll long long
 string Solution::fractionToDecimal(int A, int B) {
     
@@ -55,5 +56,77 @@ string Solution::fractionToDecimal(int A, int B) {
     else
         res = mant + "." + cha;
     
+    return res;
+}
+
+//Method - 2
+//Optimal
+string Solution::fractionToDecimal(int A, int B) {
+    
+    bool flag = false;
+    
+    if(A < 0 && B > 0 || A > 0 && B < 0){
+        flag = true;
+    }
+    
+    long long a,b;
+    
+    a = A;
+    b = B;
+    
+    a = abs(a);
+    b = abs(b);
+    
+    string res;
+    
+    res += to_string(a/b);
+    
+    a %= b;
+    
+    if(a == 0){
+        return res;
+    }
+    
+    unordered_map<int,int> umap;
+    
+    res.push_back('.');
+
+    int ind = 0;
+    
+    while(a){
+
+         if(umap.find(a) != umap.end()){
+             ind = umap[a];
+             break;
+         }
+         else{
+             umap[a] = ind;
+         }
+         
+         a *= 10;
+         res.push_back(a/b + '0');
+         a %= b;
+         ind++;
+    }
+    
+    int i = 0;
+    
+    while(res[i] != '.' && i < res.size()){
+        i++;
+    }
+    
+    i++;
+    while(ind){
+        ind--;
+        i++;
+    }
+    
+    if(i < res.size()){
+        res.insert(res.begin() + i,'(');
+        res.push_back(')');
+    }
+    if(flag){
+        res.insert(res.begin(),'-');
+    }
     return res;
 }
