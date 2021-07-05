9f85de7ba6d96ea53b3eeea69fb7d6e1391ec8d2
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-07-05 16:49:09 +0530

    Update Least Common Ancestor - Better.cpp

--- Tree Data Structure/Least Common Ancestor - Better.cpp
+++ Tree Data Structure/Least Common Ancestor - Better.cpp
@@ -1,4 +1,5 @@
 //Ref: https://www.youtube.com/watch?v=T6Jr-Q6bvSU
+//Method - 1
 /**
  * Definition for binary tree
  * struct TreeNode {
@@ -49,5 +50,54 @@ int Solution::lca(TreeNode* A, int B, int C) {
     if(find(A,B) && find(A,C)){
         return solve(A,B,C)->val;
     }
+    return -1;
+}
+
+//Method - 2
+//Optimal
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+TreeNode* solve(TreeNode* A, int B, int C, bool &f1, bool &f2){
+
+    if(A == NULL){
+        return NULL;
+    }
+
+    if(A -> val == B){
+        f1 = true;
+    }
+    if(A -> val == C){
+        f2 = true;
+    }
+    TreeNode* l = solve(A -> left, B, C,f1,f2);
+    TreeNode* r = solve(A -> right, B, C,f1,f2);
+
+    if(A -> val == B || A -> val == C){
+        return A;
+    }
+    
+    if(l && r){
+        return A;
+    }
+    if(l) return l;
+    if(r) return r;
+    return NULL;
+}
+int Solution::lca(TreeNode* A, int B, int C) {
+    
+    bool f1 = false, f2 = false;
+    
+    TreeNode* temp = solve(A,B,C,f1,f2);
+
+    if(f1 && f2)
+    return temp -> val;
+    
     return -1;
 }
