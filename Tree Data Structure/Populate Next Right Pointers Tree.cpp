1ed8e8b69f0f4eddecab78f20929893f82feea76
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-07-05 10:14:50 +0530

    Update Populate Next Right Pointers Tree.cpp

--- Tree Data Structure/Populate Next Right Pointers Tree.cpp
+++ Tree Data Structure/Populate Next Right Pointers Tree.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 /**
  * Definition for binary tree with next pointer.
  * struct TreeLinkNode {
@@ -43,4 +44,42 @@ void Solution::connect(TreeLinkNode* A) {
                 q.push({curr->right,d+1});
             }
         }
+}
+
+//Method - 2
+/**
+ * Definition for binary tree with next pointer.
+ * struct TreeLinkNode {
+ *  int val;
+ *  TreeLinkNode *left, *right, *next;
+ *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+ * };
+ */
+void Solution::connect(TreeLinkNode* A) {
+
+    queue<TreeLinkNode*> q;
+
+    q.push(A);
+
+    while(!q.empty()){
+
+        int sz = q.size();
+
+        while(sz != 1){
+
+            TreeLinkNode* temp1 = q.front();
+            q.pop();
+            TreeLinkNode* temp2 = q.front();
+            temp1 -> next = temp2;
+
+            if(temp1 -> left) q.push(temp1 -> left);
+            if(temp1 -> right) q.push(temp1 -> right);
+            sz--;
+        }
+        TreeLinkNode* temp1 = q.front();
+        q.pop();
+        if(temp1 -> left) q.push(temp1 -> left);
+        if(temp1 -> right) q.push(temp1 -> right);
+        temp1 -> next = NULL;
+    }
 }
