c4c53afda7e8909cdc80cdc8321fc9ce7b4d388c
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-07-07 14:28:32 +0530

    Update Path with good nodes!.cpp

--- Graph/Path with good nodes!.cpp
+++ Graph/Path with good nodes!.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 void dfs(int node, int par, vector<int> &val, int &count, vector<int> al[], vector<int> &A, vector<int> &vis, int C){
     
     vis[node] = 1;
@@ -38,4 +39,50 @@ int Solution::solve(vector<int> &A, vector<vector<int> > &B, int C) {
     dfs(1,-1,val,count,al,A,vis,C);
     
     return count;
+}
+
+//Method - 2
+void dfs(int node, int &tcount, int count, vector<int> &A, vector<int> al[], int C, vector<int> &vis){
+    
+    vis[node] = 1;
+    if(al[node].size() == 1){
+        if(count <= C){
+            tcount++;
+        }
+        return;
+    }
+    for(int i = 0; i < al[node].size(); i++){
+
+        int child = al[node][i];
+
+        if(vis[child] == 0 && A[child-1] == 1){
+            dfs(child,tcount,count+1,A,al,C,vis);
+        }
+        else if(vis[child] == 0){
+            dfs(child,tcount,count,A,al,C,vis);
+        }
+    }
+    vis[node] = 0;      //Not Required
+}
+int Solution::solve(vector<int> &A, vector<vector<int> > &B, int C) {
+
+    int n = A.size();
+
+    vector<int> al[n+1];
+
+    for(int i = 0; i < B.size(); i++){
+        int a = B[i][0], b = B[i][1];
+        al[a].push_back(b);
+        al[b].push_back(a);
+    }
+
+    int tcount = 0, count;
+    
+    vector<int> vis(n+1,0);
+    
+    if(A[0] == 1) count++;
+    
+    dfs(1,tcount,count,A,al,C,vis);
+
+    return tcount;
 }
