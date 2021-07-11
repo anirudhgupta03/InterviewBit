7648d1ad2b7ea67e3bfe5f848cf1a66b18dc4230
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-07-11 17:05:30 +0530

    Update Word Break II.cpp

--- Dynamic Programming/Word Break II.cpp
+++ Dynamic Programming/Word Break II.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 vector<string> Solution::wordBreak(string A, vector<string> &B) {
 
     unordered_map<string,int> umap;
@@ -40,6 +41,57 @@ vector<string> Solution::wordBreak(string A, vector<string> &B) {
     
     for(int j = 0; j < dp2[n].size(); j++){
         res.push_back(dp2[n][j]);
+    }
+    sort(res.begin(),res.end());
+    return res;
+}
+
+//Method - 2
+vector<string> Solution::wordBreak(string A, vector<string> &B) {
+
+    unordered_map<string,int> dict;
+
+    for(int i = 0; i < B.size(); i++){
+        dict[B[i]]++;
+    }
+
+    vector<string> res;
+    int n = A.size();
+    int dp1[n];
+
+    memset(dp1,0,sizeof(dp1));
+    vector<string> dp2[n];
+
+    for(int i = 0; i < n; i++){
+        for(int j = i; j >= 0; j--){
+
+            string temp = A.substr(j,i-j+1);
+
+            if(dict.find(temp) != dict.end()){
+                
+                if(j >= 1){
+                    if(dp1[j-1]){
+                        dp1[i] += dp1[j-1];
+                        vector<string> v = dp2[j-1];
+
+                        for(int k = 0; k < v.size(); k++){
+                            dp2[i].push_back(v[k] + " " + temp);
+                        }
+                    }
+                    else{
+                        continue;
+                    }
+                }
+                else{
+                    dp1[i] = 1;
+                    dp2[i].push_back(temp);
+                }
+            }
+        }
+    }
+
+    if(dp1[n-1]){
+        res = dp2[n - 1];
     }
     sort(res.begin(),res.end());
     return res;
