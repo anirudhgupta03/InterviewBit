6503360557485cdd32ccd532977a50677c23fc51
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-08-02 23:38:56 +0530

    Update Paint House!.cpp

--- Dynamic Programming/Paint House!.cpp
+++ Dynamic Programming/Paint House!.cpp
@@ -27,3 +27,37 @@ int Solution::solve(vector<vector<int> > &A) {
     
     return min(paint(n,0,A,dp), min(paint(n,1,A,dp), paint(n,2,A,dp)));
 }
+
+//Top-Down
+int paint(int ind, int col, vector<vector<int>> &A,  vector<vector<int>> &dp){
+
+    if(ind == A.size() - 1){
+        return A[ind][col];
+    }
+    
+    if(dp[ind][col] != -1){
+        return dp[ind][col];
+    }
+    
+    int ans = INT_MAX;
+    
+    for(int i = 0; i < 3; i++){
+        if(i == col) continue;
+        ans = min(ans, paint(ind+1, i, A, dp) + A[ind][col]);
+    }
+    
+    return dp[ind][col] = ans;
+}
+int Solution::solve(vector<vector<int> > &A) {
+
+    int n = A.size();
+    
+    vector<vector<int>> dp(n,vector<int>(3,-1));
+    
+    int minCost = INT_MAX;
+
+    for(int i = 0; i < 3; i++){
+        minCost = min(minCost, paint(0,i,A,dp));
+    }
+    return minCost;
+}
