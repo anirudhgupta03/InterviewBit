67bf7f5565838607cd5dc860874af7d7a70c273e
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-08-03 17:06:27 +0530

    Update Maximum Path in Triangle.cpp

--- Dynamic Programming/Maximum Path in Triangle.cpp
+++ Dynamic Programming/Maximum Path in Triangle.cpp
@@ -18,3 +18,17 @@ int Solution::solve(vector<vector<int> > &A) {
 
     return maxPathTriangle(0,0,A,dp);
 }
+
+//Alter
+int Solution::solve(vector<vector<int> > &A) {
+
+    int n = A.size();
+
+    for(int i = n - 2; i >= 0; i--){
+        for(int j = 0; j <= i; j++){
+            A[i][j] += max(A[i+1][j], A[i+1][j+1]);
+        }
+    }
+
+    return A[0][0];
+}
