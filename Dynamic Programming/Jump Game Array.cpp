274de7bc885d9e62d1e24e2809582dc3f8585319
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-08-03 09:13:43 +0530

    Update Jump Game Array.cpp

--- Dynamic Programming/Jump Game Array.cpp
+++ Dynamic Programming/Jump Game Array.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 int Solution::canJump(vector<int> &A) {
     
     int n = A.size();
@@ -26,4 +27,32 @@ int Solution::canJump(vector<int> &A) {
         }
     }
     return dp[n-1];
+}
+
+//Method - 2
+//Better Approach
+int Solution::canJump(vector<int> &A) {
+
+    int n = A.size();
+
+    if(n == 1){
+        return 1;
+    }
+    else if(A[0] == 0){
+        return 0;
+    }
+    int maxR = A[0];
+    for(int i = 1; i < n; i++){
+
+        if(maxR >= i){
+            maxR = max(maxR, i + A[i]);
+        }
+        else{
+            return 0;
+        }
+
+        if(maxR >= n - 1){
+            return 1;
+        }
+    }
 }
