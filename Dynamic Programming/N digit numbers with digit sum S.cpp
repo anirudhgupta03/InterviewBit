b99516a1b51c1a129a7ed37c47c2b226b6b78a3c
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-08-03 11:23:14 +0530

    Update N digit numbers with digit sum S.cpp

--- Dynamic Programming/N digit numbers with digit sum S.cpp
+++ Dynamic Programming/N digit numbers with digit sum S.cpp
@@ -30,3 +30,49 @@ int Solution::solve(int A, int B) {
     
     return recur(sum, count, A, B, dp, mod);
 }
+
+//Method - 2
+#define ll long long
+ll recur(int A, int B, vector<vector<ll>> &dp, int mod){
+ 
+    if(A == 0 && B == 0){
+        return 1;
+    }
+    if(A == 0){             //if(B == 0) we can't put this here as we can put in all remaining places zero
+        return 0;
+    }
+ 
+    if(dp[A][B] != -1){
+        return dp[A][B];
+    }
+ 
+    ll ans = 0;
+    for(int i = 0; i <= 9; i++){
+        if(B >= i){
+            ans = (ans + recur(A-1,B-i,dp,mod))%mod;
+        }
+    }
+    return dp[A][B] = ans;
+}
+int Solution::solve(int A, int B) {
+ 
+    if(B > 9*A){
+        return 0;
+    }
+ 
+    if(B == 0){
+        return 0;
+    }
+ 
+    int mod = 1e9 + 7;
+ 
+    ll count = 0;
+    vector<vector<ll>> dp(A+1,vector<ll>(B+1,-1));
+ 
+    for(int i = 1; i <= 9; i++){
+        if(i <= B){
+            count = (count + recur(A-1,B-i,dp,mod))%mod;
+        }
+    }
+    return count%mod;
+}
