1700f2f25a0f7f1b8c6468fba01309349e7ad6cc
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-08-03 23:58:41 +0530

    Update Min Sum Path in Matrix.cpp

--- Dynamic Programming/Min Sum Path in Matrix.cpp
+++ Dynamic Programming/Min Sum Path in Matrix.cpp
@@ -36,3 +36,42 @@ int Solution::minPathSum(vector<vector<int> > &A) {
     
     return solve(0,0,A,dp);
 }
+
+//Using Dijkstra's Algo
+#define pipii pair<int,pair<int,int>>
+int dx[2] = {1,0};
+int dy[2] = {0,1};
+int Solution::minPathSum(vector<vector<int> > &A) {
+
+    int m = A.size(), n = A[0].size();
+
+    priority_queue<pipii,vector<pipii>,greater<pipii>> pq;
+    pq.push({A[0][0],{0,0}});
+
+    vector<vector<int>> dist(m,vector<int>(n,INT_MAX));
+    
+    dist[0][0] = A[0][0];
+    vector<vector<int>> vis(m,vector<int>(n,0));
+
+    while(!pq.empty()){
+
+        int d = pq.top().first, x = pq.top().second.first, y = pq.top().second.second;
+        pq.pop();
+
+        if(vis[x][y]){
+            continue;
+        }
+        vis[x][y] = 1;
+
+        for(int i = 0; i < 2; i++){
+            int xo = x + dx[i], yo = y + dy[i];
+            if(xo < m && yo < n && vis[xo][yo] == 0){
+                if(d + A[xo][yo] < dist[xo][yo]){
+                    dist[xo][yo] = d + A[xo][yo];
+                    pq.push({dist[xo][yo],{xo,yo}});
+                }
+            }
+        }
+    }
+    return dist[m-1][n-1];
+}
