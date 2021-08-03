03af3feb80612e8d6f17ae96dde438fdefc1e452
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-08-03 09:17:57 +0530

    Update Min Jumps Array.cpp

--- Dynamic Programming/Min Jumps Array.cpp
+++ Dynamic Programming/Min Jumps Array.cpp
@@ -1,3 +1,6 @@
+//Method - 1
+//Time Complexity - O(N^2)
+//Space Complexity - O(N)
 int Solution::jump(vector<int> &A) {
     
     int n = A.size();
@@ -33,4 +36,41 @@ int Solution::jump(vector<int> &A) {
         return -1;
     }
     return dp[n-1];
+}
+
+//Method - 2
+//Time Complexity - O(N)
+//Space Complexity - O(1)
+int Solution::jump(vector<int> &A) {
+
+    int n = A.size();
+
+    if(n == 1){
+        return 0;
+    }
+    else if(A[0] == 0){
+        return -1;
+    }
+
+    int maxR = A[0], steps = A[0], jumps = 0;
+
+    for(int i = 1; i < n; i++){
+        
+        if(i == n - 1){
+            return jumps + 1;
+        }
+        steps--;
+        maxR = max(maxR, i + A[i]);
+
+        if(steps == 0){
+
+            if(maxR > i){
+                steps = maxR - i;
+                jumps++;
+            }
+            else{
+                return -1;
+            }
+        }
+    }
 }
