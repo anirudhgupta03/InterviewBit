fb3c6ae3f9320fc3f5df6f27df59cd5620c4342b
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-08-04 16:51:53 +0530

    Update Tushar's Birthday Party.cpp

--- Dynamic Programming/Tushar's Birthday Party.cpp
+++ Dynamic Programming/Tushar's Birthday Party.cpp
@@ -87,3 +87,48 @@ int Solution::solve(const vector<int> &A, const vector<int> &B, const vector<int
     
     return minCost;
 }
+//Method - 3
+//Best
+int Solution::solve(const vector<int> &A, const vector<int> &B, const vector<int> &C) {
+
+    int W = INT_MIN;
+
+    for(int i = 0; i < A.size(); i++){
+        W = max(W, A[i]);
+    }
+
+    int n = B.size();
+
+    int dp[n+1][W+1];
+
+    for(int i = 0; i <= n; i++){
+        for(int j = 0; j <= W; j++){
+            if(j == 0){
+                dp[i][j] = 0;
+            }
+            else if(i == 0){
+                dp[i][j] = INT_MAX;
+            }
+            else if(B[i-1] <= j){
+                int temp = dp[i][j-B[i-1]];
+                if(temp != INT_MAX){
+                    dp[i][j] = min(temp + C[i-1], dp[i-1][j]);
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+            else{
+                dp[i][j] = dp[i-1][j];
+            }
+        }
+    }
+
+    int minCost = 0;
+
+    for(int i = 0; i < A.size(); i++){
+        minCost += dp[n][A[i]];
+    }
+
+    return minCost;
+}
