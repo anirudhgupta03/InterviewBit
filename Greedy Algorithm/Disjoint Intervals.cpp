9f93893665c80eceb585464a452c7891f8070b0b
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-08-21 18:47:50 +0530

    Update Disjoint Intervals.cpp

--- Greedy Algorithm/Disjoint Intervals.cpp
+++ Greedy Algorithm/Disjoint Intervals.cpp
@@ -1,4 +1,26 @@
+//TLE
 bool cmp(vector<int> v1, vector<int> v2){
+    return v1[1] < v2[1];
+}
+int Solution::solve(vector<vector<int> > &A) {
+    
+    sort(A.begin(),A.end(),cmp);
+    
+    int count = 1;
+    
+    int start = A[0][0], end = A[0][1];
+    
+    for(int i = 1; i < A.size(); i++){
+        
+        if(A[i][0] > end){
+            count++;
+            end = A[i][1];
+        }
+    }
+    return count;
+}
+//Accepted
+bool cmp(vector<int> &v1, vector<int> &v2){         //Pass by reference
     return v1[1] < v2[1];
 }
 int Solution::solve(vector<vector<int> > &A) {
