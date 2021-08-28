2a028b3a23123d15047f57faedf21522788408aa
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-08-28 10:43:04 +0530

    Update Seats.cpp

--- Greedy Algorithm/Seats.cpp
+++ Greedy Algorithm/Seats.cpp
@@ -34,3 +34,39 @@ int Solution::seats(string A) {
     }
     return jumps%10000003;
 }
+
+int Solution::seats(string A) {
+
+    vector<int> pos;
+
+    for(int i = 0; i < A.size(); i++){
+        if(A[i] == 'x'){
+            pos.push_back(i);
+        }
+    }
+
+    if(pos.size() <= 1){
+        return 0;
+    }
+    int mid = pos.size()/2;
+
+    int jumps = 0, mod = 10000003;
+
+    int p1 = pos[mid], p2 = mid;
+    
+    while(p1 >= 0 && p2 >= 0){
+        jumps = (jumps + (p1 - pos[p2]))%mod;
+        p1--;
+        p2--;
+    }
+
+    p1 = pos[mid] + 1, p2 = mid + 1;
+
+    while(p1 < A.size() && p2 < pos.size()){
+        jumps = (jumps + (pos[p2] - p1))%mod;
+        p2++;
+        p1++;
+    }
+
+    return jumps;
+}
