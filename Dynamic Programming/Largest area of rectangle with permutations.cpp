40e0fbbd683c7fce3b6d00a13fb945f714670d4f
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-03-11 12:09:11 +0530

    Update Largest area of rectangle with permutations.cpp

--- Dynamic Programming/Largest area of rectangle with permutations.cpp
+++ Dynamic Programming/Largest area of rectangle with permutations.cpp
@@ -1,28 +1,21 @@
 //Ref: https://www.youtube.com/watch?v=9vYCQLYF6ng&t=768s
+//TC - O(n*m*log(m))
+//SC - O(1)
 int Solution::solve(vector<vector<int> > &A) {
-    
-    for(int j = 0; j < A[0].size(); j++){
-        for(int i = 1; i < A.size(); i++){
-            if(A[i][j] != 0)
-            A[i][j] += A[i-1][j];
+    int n = A.size(), m = A[0].size();
+    for(int i = 1; i < n; i++){
+        for(int j = 0; j < m; j++){
+            if(A[i][j]){
+                A[i][j] += A[i - 1][j];
+            }
         }
     }
-    
-    for(int i = 0; i < A.size(); i++){
-        sort(A[i].begin(),A[i].end());
-    }
-    
     int maxArea = 0;
-    
-    for(int i = 0; i < A.size(); i++){
-        
-        for(int j = A[0].size() - 1; j >= 0; j--){
-            
-            int area = A[i][j]*(A[0].size() - j);
-
-            maxArea = max(maxArea,area);
+    for(int i = 0; i < n; i++){
+        sort(A[i].begin(),A[i].end());
+        for(int j = 0; j < m; j++){
+            maxArea = max(maxArea, A[i][j]*(m - j));
         }
     }
-    
     return maxArea;
 }
