b2913f2d9ee5c94b52af0ba8690541093dc3a4b0
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-03-11 11:05:24 +0530

    Update Length of Longest Subsequence.cpp

--- Dynamic Programming/Length of Longest Subsequence.cpp
+++ Dynamic Programming/Length of Longest Subsequence.cpp
@@ -1,4 +1,7 @@
 //Find LIS for original array and reverse array
+//Method - 1
+//TC - O(n*n)
+//SC - O(n)
 int Solution::longestSubsequenceLength(const vector<int> &A) {
     
     int n = A.size();
@@ -28,4 +31,52 @@ int Solution::longestSubsequenceLength(const vector<int> &A) {
     }
     
     return ans;
+}
+
+//Method - 2
+//TC - O(n*log(n))
+//SC - O(n)
+int Solution::longestSubsequenceLength(const vector<int> &A) {
+    
+    int len = A.size();
+    if(len == 0){
+        return 0;
+    }
+    vector<int> dp1(len,1), dp2(len,1);
+    
+    vector<int> temp;
+    temp.push_back(A[0]);
+    for(int i = 1; i < len; i++){
+        if(A[i] > temp.back()){
+            temp.push_back(A[i]);
+        }
+        else{
+            int ind  = lower_bound(temp.begin(), temp.end(), A[i]) - temp.begin();
+            if(ind != temp.size()){
+                temp[ind] = A[i];
+            }
+        }
+        dp1[i] = temp.size();
+    }
+    
+    temp.clear();
+    temp.push_back(A[len - 1]);
+
+    for(int i = len - 2; i >= 0; i--){
+        if(A[i] > temp.back()){
+            temp.push_back(A[i]);
+        }
+        else{
+            int ind  = lower_bound(temp.begin(), temp.end(), A[i]) - temp.begin();
+            if(ind != temp.size()){
+                temp[ind] = A[i];
+            }
+        }
+        dp2[i] = temp.size();
+    }
+    int maxLen = 0;
+    for(int i = 0; i < len; i++){
+        maxLen = max(maxLen, dp1[i] + dp2[i] - 1);
+    }
+    return maxLen;
 }
