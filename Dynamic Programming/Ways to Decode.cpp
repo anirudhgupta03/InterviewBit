b9f46363a5acbbef468a5e0c88222fe6dea4d5da
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-03-11 15:32:44 +0530

    Update Ways to Decode.cpp

--- Dynamic Programming/Ways to Decode.cpp
+++ Dynamic Programming/Ways to Decode.cpp
@@ -52,3 +52,55 @@ int Solution::numDecodings(string A) {
     }
     return dp[n-1]%mod;
 }
+//TC - O(n)
+//SC - O(n)
+#define ll long long
+int Solution::numDecodings(string A) {
+
+    int mod = 1e9 + 7;
+    int n = A.size();
+
+    if(A[0] == '0'){
+        return 0;
+    }
+    vector<ll> dp(n);
+    dp[0] = 1;
+
+    for(int i = 1; i < n; i++){
+        if(A[i] == '0'){
+            if(A[i - 1] == '1' || A[i - 1] == '2'){
+                if(i - 2 >= 0){
+                    dp[i] = dp[i - 2];
+                }
+                else{
+                    dp[i] = 1;
+                }
+            }
+            else{
+                dp[i] = 0;
+            }
+        }
+        else{
+            dp[i] = dp[i - 1];
+            if(A[i - 1] == '1'){
+                if(i - 2 >= 0){
+                    dp[i] = (dp[i] + dp[i - 2])%mod;
+                }
+                else{
+                    dp[i] = (dp[i] + 1)%mod;
+                }
+            }
+            else if(A[i - 1] == '2'){
+                if(A[i] >= '0' && A[i] <= '6'){
+                    if(i - 2 >= 0){
+                    dp[i] = (dp[i] + dp[i - 2])%mod;
+                    }
+                    else{
+                        dp[i] = (dp[i] + 1)%mod;
+                    }
+                }
+            }
+        }
+    }
+    return dp[n - 1]%mod;
+}
