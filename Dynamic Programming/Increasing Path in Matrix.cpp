c69bd9d1c0ff7bbe33a42fe1a023bc5e5ec94b52
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-03-12 14:22:18 +0530

    Update Increasing Path in Matrix.cpp

--- Dynamic Programming/Increasing Path in Matrix.cpp
+++ Dynamic Programming/Increasing Path in Matrix.cpp
@@ -1,4 +1,6 @@
 //Top-Down Approach
+//TC - O(m*n)
+//SC - O(m*n)
 int dx[2] = {0,1};
 int dy[2] = {1,0};
 int incrPath(int x, int y, vector<vector<int>> &A, vector<vector<int>> &dp){
@@ -41,4 +43,32 @@ int Solution::solve(vector<vector<int> > &A) {
         return -1;
     }
     return temp + 1;
+}
+
+//Alter
+int dx[2] = {0,1};
+int dy[2] = {1,0};
+int dp[1001][1001];
+int rec(int x, int y, int n, int m, vector<vector<int>> &A){
+
+    if(x == n - 1 && y == m - 1){
+        return 1;
+    }
+    if(dp[x][y] != -1){
+        return dp[x][y];
+    }
+    int maxLen = -1;
+    for(int i = 0; i < 2; i++){
+        int xo = x + dx[i], yo = y + dy[i];
+        if(xo >= 0 && yo >= 0 && xo < n && yo < m && A[xo][yo] > A[x][y]){
+            int temp = rec(xo, yo, n, m, A);
+            if(temp != -1) maxLen = max(maxLen,temp + 1);
+        }
+    }
+    return dp[x][y] = maxLen;
+}
+int Solution::solve(vector<vector<int> > &A) {
+    int n = A.size(), m = A[0].size();
+    memset(dp,-1,sizeof(dp));
+    return rec(0,0,n,m,A);
 }
