0cd6c921c29a582c1144f531b79ff0d823d62fd8
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-03-12 14:01:23 +0530

    Update and rename Maximum Size Square Sub-matrix to Maximum Size Square Sub-matrix.cpp

--- Dynamic Programming/Maximum Size Square Sub-matrix.cpp
+++ Dynamic Programming/Maximum Size Square Sub-matrix.cpp
@@ -1,4 +1,6 @@
 //Ref: https://www.youtube.com/watch?v=UagRoA3C5VQ
+//TC - O(n*m)
+//SC - O(n*m)
 int Solution::solve(vector<vector<int> > &A) {
     
     int n = A.size(), m = A[0].size();
@@ -11,7 +13,6 @@ int Solution::solve(vector<vector<int> > &A) {
     
     for(int i = 0; i < n; i++){
         for(int j = 0; j < m; j++){
-            
             if(i == n - 1 || j == m - 1){
                 if(A[i][j] == 1){
                     dp[i][j] = 1;
@@ -24,10 +25,8 @@ int Solution::solve(vector<vector<int> > &A) {
             
         }
     }
-    
     for(int i = n - 2; i >= 0; i--){
-        for(int j = m - 2; j >= 0; j--){
-            
+        for(int j = m - 2; j >= 0; j--){       
             if(A[i][j]){
                 dp[i][j] = min({dp[i][j+1],dp[i+1][j],dp[i+1][j+1]}) + 1;
                 maxSize = max(maxSize, dp[i][j]);
