4e5216bad5b6e68187e38a99cd3958dcba8f2db0
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-03-13 17:28:56 +0530

    Update Arrange II.cpp

--- Dynamic Programming/Arrange II.cpp
+++ Dynamic Programming/Arrange II.cpp
@@ -1,51 +1,43 @@
+//Top-Down Approach
+//TC - O(n*B)
+//SC - O(n*B)
 int solve(int ind, string A, int B, int w[], int b[], vector<vector<int>> &dp){
     
     if(ind == A.size() && B == 0){
         return 0;
     }
-    
     if(ind == A.size() || B == 0){
         return INT_MAX;
     }
-    
     if(dp[ind][B] != -1){
         return dp[ind][B];
     }
     
     int mn = INT_MAX;
-    
     for(int i = ind; i < A.size(); i++){
-        
         int temp = solve(i+1,A,B - 1,w,b,dp);
-        
         if(temp != INT_MAX){
             temp += (w[i+1]-w[ind])*(b[i+1]-b[ind]);
         }
-        
         mn = min(mn,temp);
     }
-    
     return dp[ind][B] = mn;
 }
 int Solution::arrange(string A, int B) {
     
     int n = A.size();
-    
     if(B > n){
         return -1;
     }
-    
     if(B == n){
         return 0;
     }
-    
     int w[n+1], b[n+1];
     memset(w,0,sizeof(w));
     memset(b,0,sizeof(b));
     
     w[0] = 0;
     b[0] = 0;
-    
     for(int i = 1; i <= n; i++){
         
         if(A[i - 1] == 'W'){
