1c3424867921d306a10210247789092eb41f5774
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-03-13 16:42:38 +0530

    Update Coin Sum Infinite.cpp

--- Dynamic Programming/Coin Sum Infinite.cpp
+++ Dynamic Programming/Coin Sum Infinite.cpp
@@ -1,4 +1,6 @@
 //Ref: https://www.youtube.com/watch?v=l_nR5X9VmaI
+//TC - O(n*B)
+//SC - O(B)
 int Solution::coinchange2(vector<int> &A, int B) {
     
     int mod = 1000007;
@@ -15,4 +17,38 @@ int Solution::coinchange2(vector<int> &A, int B) {
         }
     }
     return dp[B]%mod;
+}
+
+//Alter
+//TC - O(n*B)
+//SC - O(2*B)
+int Solution::coinchange2(vector<int> &A, int B) {
+
+    int n = A.size();
+    int dp[2][B + 1];
+    memset(dp,0,sizeof(dp));
+    dp[0][0] = 1;
+    int mod = 1000007;
+
+    for(int i = 0; i <= n; i++){
+        for(int j = 0; j <= B; j++){
+            if(j == 0){
+                dp[1][j] = 1;
+            }
+            else if(i == 0){
+                dp[1][j] = 0;
+            }
+            else if(A[i - 1] <= j){
+                dp[1][j] = (dp[1][j - A[i - 1]] + dp[0][j])%mod;
+            }
+            else{
+                dp[1][j] = dp[0][j];
+            }
+        }
+        for(int j = 0; j <= B; j++){
+            dp[0][j] = dp[1][j];
+            dp[1][j] = 0;
+        }
+    }
+    return dp[0][B];
 }
