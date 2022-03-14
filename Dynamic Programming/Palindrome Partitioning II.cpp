a10f252b9f256c2981558a56091a24b9f6aa60e0
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-03-14 11:29:26 +0530

    Update Palindrome Partitioning II.cpp

--- Dynamic Programming/Palindrome Partitioning II.cpp
+++ Dynamic Programming/Palindrome Partitioning II.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 bool ispalindrome(string s,int start, int end){
         
         while(start < end){
@@ -62,4 +63,79 @@ int Solution::minCut(string str) {
         }
         
         return solve(str,0,n-1,dp);
+}
+
+//Method - 2
+int minCuts;
+int dp[502];
+bool isPalindrome(string s){
+    int l = 0, r = s.size() - 1;
+    while(l < r){
+        if(s[l] != s[r]){
+            return false;
+        }
+        l++;
+        r--;
+    }
+    return true;
+}
+int solve(int ind, string &A){
+    if(ind == A.size()){
+        return -1;
+    }
+    if(dp[ind] != -1){
+        return dp[ind];
+    }
+    int minCuts = INT_MAX;
+    for(int i = ind; i < A.size(); i++){
+        if(isPalindrome(A.substr(ind, i - ind + 1))){
+            minCuts = min(minCuts, solve(i + 1, A) + 1);
+        }
+    }
+    return dp[ind] = minCuts;
+}
+int Solution::minCut(string A) {
+    memset(dp,-1,sizeof(dp));
+    return solve(0,A);
+}
+
+//Method - 3
+//Best
+int dp[502];
+int solve(int ind, string &A, vector<vector<int>> &isPalindrome){
+        if(ind == A.size()){
+            return -1;
+        }
+        if(dp[ind] != -1){
+            return dp[ind];
+        }
+        int minCuts = INT_MAX;
+        for(int i = ind; i < A.size(); i++){
+            if(isPalindrome[ind][i]){
+                minCuts = min(minCuts, solve(i + 1, A, isPalindrome) + 1);
+            }
+        }
+        return dp[ind] = minCuts;
+}
+int Solution::minCut(string s) {
+        memset(dp,-1,sizeof(dp));
+        int n = s.size();
+        vector<vector<int>> isPalindrome(n, vector<int>(n,0));
+        
+        for(int i = 0; i < n; i++){
+            isPalindrome[i][i] = 1; 
+        }
+        for(int i = 0; i < n - 1; i++){
+            if(s[i] == s[i + 1]){
+                isPalindrome[i][i + 1] = 1;
+            }
+        }
+        for(int i = n - 3; i >= 0; i--){
+            for(int j = i + 2; j < n; j++){
+                if(s[i] == s[j]){
+                    isPalindrome[i][j] = isPalindrome[i + 1][j - 1];
+                }
+            }
+        }
+        return solve(0,s,isPalindrome);
 }
