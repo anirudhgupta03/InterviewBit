2d206e53e42102e549ee834e36213aa580484d6c
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-03-16 11:12:42 +0530

    Update Path in Directed Graph.cpp

--- Graph/Path in Directed Graph.cpp
+++ Graph/Path in Directed Graph.cpp
@@ -1,3 +1,4 @@
+//Using BFS
 int bfs(int A, vector<int> al[]){
    
     vector<int> vis(A+1,0);
@@ -35,4 +36,36 @@ int Solution::solve(int A, vector<vector<int> > &B) {
     }
     
     return bfs(A,al);
+}
+
+//Using DFS
+int pathExist(int node, int A, vector<int> al[], vector<int> &vis){
+
+    if(node == A){
+        return true;
+    }
+    vis[node] = 1;
+    for(int i = 0; i < al[node].size(); i++){
+        int child = al[node][i];
+        if(vis[child] == 0){
+            bool flag = pathExist(child, A, al, vis);
+            if(flag){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+int Solution::solve(int A, vector<vector<int> > &B) {
+
+    vector<int> al[A + 1];
+
+    for(int i = 0; i < B.size(); i++){
+        int u = B[i][0], v = B[i][1];
+        al[u].push_back(v);
+    }
+
+    vector<int> vis(A + 1, 0);
+
+    return pathExist(1, A, al, vis);
 }
