6f844ec2fc030dcf0e8a44602d01449eea84592f
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-03-17 11:19:34 +0530

    Update Permutation Swaps!.cpp

--- Graph/Permutation Swaps!.cpp
+++ Graph/Permutation Swaps!.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 bool isReachable(int src, int dest, unordered_map<int,vector<int>> &umap, unordered_map<int,int> &vis){
     
     if(src == dest){
@@ -36,4 +37,53 @@ int Solution::solve(vector<int> &A, vector<int> &B, vector<vector<int> > &C) {
         }
     }
     return true;
+}
+
+//Method - 2
+void rec(int curr, int parent, vector<int> &vis,  vector<int> al[], vector<int> &A, unordered_set<int> &us, vector<int> &par){
+    us.insert(A[curr]);
+    vis[curr] = 1;
+    par[curr] = parent;
+
+    for(int i = 0; i < al[curr].size(); i++){
+        int child = al[curr][i];
+        if(vis[child] == 0){
+            rec(child, parent, vis, al, A, us, par);
+        }
+    }
+}
+int Solution::solve(vector<int> &A, vector<int> &B, vector<vector<int> > &C) {
+    int n = A.size();
+
+    vector<int> al[n];
+    for(int i = 0; i < C.size(); i++){
+        int u = C[i][0], v = C[i][1];
+        al[u - 1].push_back(v - 1);
+        al[v - 1].push_back(u - 1);
+    }
+
+    vector<int> vis(n, 0);
+    vector<unordered_set<int>> vec(n);
+    vector<int> par(n);
+
+    for(int i = 0; i < n; i++){
+        par[i] = i;
+    }
+
+    for(int i = 0; i < n; i++){
+        if(vis[i] == 0){
+            unordered_set<int> us;
+            rec(i, i, vis, al, A, us, par);
+            vec[i] = us;
+        }
+    }
+    for(int i = 0; i < n; i++){
+        if(vec[par[i]].find(B[i]) == vec[par[i]].end()){
+            return 0;
+        }
+        else{
+            vec[par[i]].erase(B[i]);
+        }
+    }
+    return 1;
 }
