2d0931381697116ea6f7aedf807faa9b3da3c60c
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-03-17 09:32:41 +0530

    Update Valid Path.cpp

--- Graph/Valid Path.cpp
+++ Graph/Valid Path.cpp
@@ -1,4 +1,5 @@
 //Ref: https://www.youtube.com/watch?v=In6MrAYjeZw
+//Using BFS
 int dx[8] = {1,-1,0,0,1,1,-1,-1};
 int dy[8] = {0,0,1,-1,1,-1,-1,1};
 
@@ -50,5 +51,50 @@ string Solution::solve(int A, int B, int C, int D, vector<int> &E, vector<int> &
     if(vis[A][B]){
         return "YES";
     }
+    return "NO";
+}
+
+//Using DFS
+int dx[4] = {-1,1,0,0};
+int dy[4] = {0,0,-1,1};
+bool rec(int x, int y, vector<vector<int>> &grid){
+
+    grid[x][y] = 1;
+
+    if(x == grid.size() - 1 && y == grid[0].size() - 1){
+        return true;
+    }
+    for(int i = 0; i < 4; i++){
+        int xo = x + dx[i], yo = y + dy[i];
+        if(xo >= 0 && yo >= 0 && xo < grid.size() && yo < grid[0].size()){
+            if(grid[xo][yo] == 0){
+                bool flag = rec(xo, yo, grid);
+                if(flag) return true;
+            }
+        }
+    }
+    return false;
+}
+string Solution::solve(int A, int B, int C, int D, vector<int> &E, vector<int> &F) {
+
+    vector<vector<int>> grid(A + 1, vector<int>(B + 1, 0));
+
+    for(int i = 0; i <= A; i++){
+        for(int j = 0; j <= B; j++){
+
+            for(int k = 0; k < C; k++){
+                int x1 = i, x2 = E[k], y1 = j, y2 = F[k];
+                if((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1) <= D*D){
+                    grid[x1][y1] = 1;
+                    break;
+                }
+            }
+        }
+    }
+    if(grid[0][0] == 1 || grid[A][B] == 1){
+        return "NO";
+    }
+    bool flag = rec(0,0,grid);
+    if(flag) return "YES";
     return "NO";
 }
