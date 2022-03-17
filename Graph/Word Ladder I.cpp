252dcadbc51c212267e3cfa1b4dee6279abfc1c2
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-03-17 14:45:16 +0530

    Update Word Ladder I.cpp

--- Graph/Word Ladder I.cpp
+++ Graph/Word Ladder I.cpp
@@ -33,3 +33,42 @@ int Solution::solve(string A, string B, vector<string> &C) {
     }
     return 0;
 }
+
+//Alter
+int Solution::solve(string A, string B, vector<string> &C) {
+    if(A == B){
+        return 0;
+    }
+    unordered_set<string> valid, vis;
+    for(int i = 0; i < C.size(); i++){
+        valid.insert(C[i]);
+    }
+    valid.insert(B);
+    queue<string> q;
+    q.push(A);
+    int steps = 1;
+    while(!q.empty()){
+        int sz = q.size();
+        while(sz--){
+            string curr = q.front();
+            q.pop();
+            if(curr == B){
+                return steps;
+            }
+            if(vis.find(curr) != vis.end()) continue;
+            vis.insert(curr);
+            for(int i = 0; i < curr.size(); i++){
+                char ch = curr[i];
+                for(int j = 0; j < 26; j++){
+                    curr[i] = (j + 'a');
+                    if(valid.find(curr) != valid.end() && vis.find(curr) == vis.end()){
+                        q.push(curr);
+                    }
+                }
+                curr[i] = ch;
+            }
+        }
+        steps++;
+    }
+    return 0;
+}
