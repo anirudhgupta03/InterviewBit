85df3cf5b839198f7e2f3844dfc69bccd3a3aa2b
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-03-17 16:34:37 +0530

    Update Word Ladder 2.cpp

--- Graph/Word Ladder 2.cpp
+++ Graph/Word Ladder 2.cpp
@@ -115,3 +115,119 @@ vector<vector<string> > Solution::findLadders(string beginWord, string endWord, 
         
         return ans;
 }
+
+//Alter
+//Ref: https://www.youtube.com/watch?v=mIZJIuMpI2M
+int isValid(string &s1, string &s2){
+    int count = 0;
+    for(int i = 0; i < s1.size(); i++){
+        if(s1[i] != s2[i]){
+            count++;
+        }
+    }
+    return count == 1 ? 1 : 0;
+}
+void dfs(int depth, int curr, int end, vector<string> &temp, vector<vector<string>> &res, vector<int> al[], vector<int> &vis, vector<string> &dict){
+
+    if(depth == 0 && curr == end){
+        res.push_back(temp);
+        return;
+    }
+    if(depth == 0 || curr == end){
+        return;
+    }
+
+    for(int i = 0; i < al[curr].size(); i++){
+        int child = al[curr][i];
+        if(vis[child] == 0){
+            vis[child] = 1;
+            temp.push_back(dict[child]);
+            dfs(depth - 1, child, end, temp, res, al, vis, dict);
+            vis[child] = 0;
+            temp.pop_back();
+        }
+    }
+}
+vector<vector<string> > Solution::findLadders(string start, string end, vector<string> &dict) {
+    
+    unordered_set<string> valid;
+
+    for(string &s: dict){
+        valid.insert(s);
+    }
+    valid.insert(start);
+    valid.insert(end);
+
+    dict.clear();
+
+    for(string s: valid){
+        dict.push_back(s);
+    }
+
+    int n = dict.size();
+    vector<int> al[n];
+
+    for(int i = 0; i < n; i++){
+        for(int j = i + 1; j < n; j++){
+            if(isValid(dict[i], dict[j])){
+                al[i].push_back(j);
+                al[j].push_back(i);
+            }
+        }
+    }
+
+    int sind, eind;
+    for(int i = 0; i < n; i++){
+        if(dict[i] == start) sind = i;
+        if(dict[i] == end) eind = i;
+    }
+
+    queue<int> q;
+    int steps = 0;
+    q.push(sind);
+
+    vector<int> vis(n, 0);
+    
+    //BFS to find the depth
+    bool flag = false;
+    while(!q.empty()){
+        int sz = q.size();
+        steps++;
+        while(sz--){
+            int currind = q.front();
+            q.pop();
+
+            if(currind == eind){
+                flag = true;
+                break;
+            }
+            if(vis[currind]) continue;
+            vis[currind] = 1;
+
+            for(int i = 0; i < al[currind].size(); i++){
+                int child = al[currind][i];
+                if(vis[child] == 0){
+                    q.push(child);
+                }
+            } 
+        }
+        if(flag){
+            break;
+        }
+    }
+
+    vector<vector<string>> res;
+    if(!flag){
+        return res;
+    }
+    vector<string> temp;
+    temp.push_back(start);
+
+    for(int i = 0; i < n; i++) vis[i] = 0;
+    vis[sind] = 1;
+     
+    //DFS
+    dfs(steps - 1, sind, eind, temp, res, al, vis, dict);
+
+    return res;
+}
