9ecaacbba241628825d2c699cd108cd40440e745
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-03-17 11:46:07 +0530

    Update Commutable Islands.cpp

--- Graph/Commutable Islands.cpp
+++ Graph/Commutable Islands.cpp
@@ -35,3 +35,43 @@ int Solution::solve(int A, vector<vector<int> > &B) {
     }
     return cost;
 }
+
+//MST using Prim's Algorithm
+#define pii pair<int,int>
+struct cmp{
+    bool operator()(pii &p1, pii &p2){
+        return p1.second > p2.second;
+    }
+};
+int Solution::solve(int A, vector<vector<int> > &B) {
+
+    vector<pii> al[A + 1];
+
+    for(int i = 0; i < B.size(); i++){
+        al[B[i][0]].push_back({B[i][1], B[i][2]});
+        al[B[i][1]].push_back({B[i][0], B[i][2]});
+    }
+    vector<int> vis(A + 1,0);
+    
+    priority_queue<pii,vector<pii>,cmp> pq;
+    pq.push({1,0});
+    
+    int minCost = 0;
+    
+    while(!pq.empty()){
+        int curr = pq.top().first, cost = pq.top().second;
+        pq.pop();
+    
+        if(vis[curr]) continue;
+        vis[curr] = 1;
+        minCost += cost;
+        
+        for(int i = 0; i < al[curr].size(); i++){
+            int child = al[curr][i].first;
+            if(vis[child] == 0){
+                pq.push({child, al[curr][i].second});
+            }
+        }
+    }
+    return minCost;
+}
