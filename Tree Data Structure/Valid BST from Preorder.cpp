49e48b11e41865813add3aab04eae2e1a45ea9d6
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-04-05 16:09:00 +0530

    Update and rename Valid BST from Preorder - Optimal.cpp to Valid BST from Preorder.cpp

--- Tree Data Structure/Valid BST from Preorder.cpp
+++ Tree Data Structure/Valid BST from Preorder.cpp
@@ -1,3 +1,32 @@
+//Method - 1
+//Ref: https://www.youtube.com/watch?v=hPVDhNfO1Ng
+//Time Complexity - O(N*N)
+//Space Complexity - O(1)
+bool helper(vector<int> &A, int l, int r){
+    
+    if(l >= r){
+        return true;
+    }
+    int i;
+    for(i = l + 1; i < r; i++){
+        if(A[i] > A[l]){
+            break;
+        }
+    }
+    for(int j = i + 1; j < r; j++){
+        if(A[j] < A[l]){
+            return false;
+        }
+    }
+    bool left = helper(A,l+1,i-1);
+    bool right = helper(A,i+1,r);
+    return left && right;
+}
+int Solution::solve(vector<int> &A) {
+    return helper(A,0,A.size() - 1);
+}
+
+//Method - 2
 //Ref: https://www.geeksforgeeks.org/check-if-a-given-array-can-represent-preorder-traversal-of-binary-search-tree/
 //Ref: https://www.youtube.com/watch?v=hPVDhNfO1Ng
 //Ref: https://www.youtube.com/watch?v=GYdC4hQSo8A
@@ -14,14 +43,11 @@ int Solution::solve(vector<int> &A) {
         if(A[i] < root){
             return 0;
         }
-        
         while(!s.empty() && A[i] > s.top()){
             root = s.top();
             s.pop();
         }
-        
         s.push(A[i]);
     }
-    
     return 1;
 }
