c3ae88c1375ac257ac1d28fb597b70b87b7134f3
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-04-06 10:14:33 +0530

    Update Balanced Binary Tree.cpp

--- Tree Data Structure/Balanced Binary Tree.cpp
+++ Tree Data Structure/Balanced Binary Tree.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 /**
  * Definition for binary tree
  * struct TreeNode {
@@ -28,4 +29,47 @@ int Solution::isBalanced(TreeNode* A) {
     solve(A,flag);
     
     return flag;
+}
+
+//Method - 2
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+int height(TreeNode* root){
+    
+    if(root == NULL){
+        return 0;
+    }
+    
+    int l = height(root->left);
+    int r = height(root->right);
+    
+    return 1 + max(l,r);
+}
+int Solution::isBalanced(TreeNode* A) {
+    
+    if(A == NULL){
+        return true;
+    }
+    
+    if(isBalanced(A->left) == NULL){
+        return false;
+    }
+    if(isBalanced(A->right) == NULL){
+        return false;
+    }
+    
+    int lh = height(A->left);
+    int rh = height(A->right);
+    
+    if(abs(lh-rh) <= 1){
+        return true;
+    }
+    return false;
 }
