7431b4d487c3f7907e6314457d1ab1309dcd812f
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-04-06 11:18:36 +0530

    Update Symmetric Binary Tree.cpp

--- Tree Data Structure/Symmetric Binary Tree.cpp
+++ Tree Data Structure/Symmetric Binary Tree.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 /**
  * Definition for binary tree
  * struct TreeNode {
@@ -51,4 +52,61 @@ int Solution::isSymmetric(TreeNode* A) {
         }
     }
     return 1;
+}
+
+//Method - 2
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+bool solve(TreeNode* root1, TreeNode* root2){
+    
+    if(root1 == NULL && root2 == NULL){
+        return true;
+    }
+    if(root1 == NULL || root2 == NULL){
+        return false;
+    }
+    
+    if(root1 -> val == root2 -> val){
+        
+        if(solve(root1 -> left,root2-> right) && solve(root1 -> right, root2->left)){
+            return true;
+        }
+    }
+    return false;
+}
+int Solution::isSymmetric(TreeNode* A) {
+    
+    return solve(A,A);
+}
+
+//Method - 3
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+bool solve(TreeNode* A, TreeNode* B){
+    if(A == NULL && B == NULL){
+        return true;
+    }
+    if(A && B){
+        if(A -> val == B -> val){
+            return solve(A -> left, B -> right) && solve(A -> right, B -> left);
+        }
+    }
+    return false;
+}
+int Solution::isSymmetric(TreeNode* A) {
+    return solve(A, A);
 }
