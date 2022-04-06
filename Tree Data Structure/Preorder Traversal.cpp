45a8dce1f2418aecefb79fb4e96594b23d587e5d
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-04-06 13:42:28 +0530

    Update Preorder Traversal.cpp

--- Tree Data Structure/Preorder Traversal.cpp
+++ Tree Data Structure/Preorder Traversal.cpp
@@ -1,3 +1,4 @@
+//Recursive
 /**
  * Definition for binary tree
  * struct TreeNode {
@@ -24,4 +25,32 @@ vector<int> Solution::preorderTraversal(TreeNode* A) {
     solve(A,v);
     
     return v;
+}
+
+//Iterative
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+vector<int> Solution::preorderTraversal(TreeNode* A) {
+
+    vector<int> res;
+    if(A == NULL){
+        return res;
+    }
+    stack<TreeNode*> st;
+    st.push(A);
+    while(!st.empty()){
+        TreeNode* curr = st.top();
+        st.pop();
+        res.push_back(curr -> val);
+        if(curr -> right) st.push(curr -> right);
+        if(curr -> left) st.push(curr -> left);
+    }
+    return res;
 }
