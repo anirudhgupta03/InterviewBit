aa1674001282d223f51494e93582d80e43fc1806
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-04-06 14:12:24 +0530

    Update Postorder Traversal.cpp

--- Tree Data Structure/Postorder Traversal.cpp
+++ Tree Data Structure/Postorder Traversal.cpp
@@ -1,3 +1,4 @@
+//Recursive 
 /**
  * Definition for binary tree
  * struct TreeNode {
@@ -7,17 +8,39 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-void solve(TreeNode* A, vector<int> &v){
-     
+void solve(TreeNode* A, vector<int> &v){     
      if(A == NULL) return;
      solve(A->left,v);
      solve(A->right,v);
      v.push_back(A -> val);
- }
+}
 vector<int> Solution::postorderTraversal(TreeNode* A) {
-    
     vector<int> v;
     solve(A, v);
-    
     return v;
+}
+
+//Iterative
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+vector<int> Solution::postorderTraversal(TreeNode* A) {
+    stack<TreeNode*> st;
+    st.push(A);
+    vector<int> res;
+    while(!st.empty()){
+        TreeNode* curr = st.top();
+        st.pop();
+        res.push_back(curr -> val);
+        if(curr -> left) st.push(curr -> left);
+        if(curr -> right) st.push(curr -> right);
+    }
+    reverse(res.begin(), res.end());
+    return res;
 }
