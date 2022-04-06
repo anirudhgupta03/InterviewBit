0578b3044c890833695d45a8e8b59fbd6382d36a
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-04-06 11:10:13 +0530

    Update Merge two Binary Tree.cpp

--- Tree Data Structure/Merge two Binary Tree.cpp
+++ Tree Data Structure/Merge two Binary Tree.cpp
@@ -24,3 +24,25 @@ TreeNode* Solution::solve(TreeNode* A, TreeNode* B) {
     }
     return A;
 }
+
+//Alter
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+TreeNode* Solution::solve(TreeNode* A, TreeNode* B) {
+    if(A && B){
+        A -> val = A -> val + B -> val;
+        A -> left = solve(A -> left, B -> left);
+        A -> right = solve(A -> right, B -> right);
+        return A;
+    }
+    if(A) return A;
+    if(B) return B;
+    return NULL;
+}
