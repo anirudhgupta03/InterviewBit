42fe9485ff94edb18727bb104f58bc6e52e88242
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-04-06 16:50:49 +0530

    Update Populate Next Right Pointers Tree.cpp

--- Tree Data Structure/Populate Next Right Pointers Tree.cpp
+++ Tree Data Structure/Populate Next Right Pointers Tree.cpp
@@ -83,3 +83,122 @@ void Solution::connect(TreeLinkNode* A) {
         temp1 -> next = NULL;
     }
 }
+
+//Method - 3
+//Best
+/**
+ * Definition for binary tree with next pointer.
+ * struct TreeLinkNode {
+ *  int val;
+ *  TreeLinkNode *left, *right, *next;
+ *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+ * };
+ */
+void Solution::connect(TreeLinkNode* A) {
+    if(A == NULL){
+        return;
+    }
+    queue<TreeLinkNode*> q;
+    q.push(A);
+    while(!q.empty()){
+        int sz = q.size();
+        TreeLinkNode* pre = NULL;
+        while(sz--){
+            TreeLinkNode* curr = q.front();
+            q.pop();
+            if(pre != NULL){
+                pre -> next = curr;
+            }
+            pre = curr;
+            if(curr -> left) q.push(curr -> left);
+            if(curr ->  right) q.push(curr -> right);
+        }
+    }
+}
+
+//Method - 4
+//Ref: https://leetcode.com/problems/populating-next-right-pointers-in-each-node/discuss/37472/A-simple-accepted-solution Comments
+//Ref: https://www.youtube.com/watch?v=KX6gz1-pjg0
+/**
+ * Definition for binary tree with next pointer.
+ * struct TreeLinkNode {
+ *  int val;
+ *  TreeLinkNode *left, *right, *next;
+ *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+ * };
+ */
+void Solution::connect(TreeLinkNode* A) {
+    
+    if(A == NULL){
+        return;
+    }
+    
+    queue<TreeLinkNode*> q;
+    q.push(A);
+    q.push(NULL);
+    
+    while(!q.empty()){
+        
+        TreeLinkNode* curr = q.front();
+        q.pop();
+        
+        if(curr == NULL){
+            if(!q.empty()){
+                q.push(NULL);
+            }
+        }
+        else{
+            curr -> next = q.front();
+            if(curr -> left != NULL){
+                q.push(curr->left);
+            }
+            if(curr -> right != NULL){
+                q.push(curr -> right);
+            }
+        }
+    }
+}
+
+//Method - 5
+/**
+ * Definition for binary tree with next pointer.
+ * struct TreeLinkNode {
+ *  int val;
+ *  TreeLinkNode *left, *right, *next;
+ *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+ * };
+ */
+//This solution by takes only O(1) Space and O(n) Time
+void Solution::connect(TreeLinkNode* A) {
+    
+    TreeLinkNode* curr = A;
+    TreeLinkNode* prev = NULL;
+    TreeLinkNode* leftmost = NULL;
+    
+    while(curr != NULL){
+        while(curr != NULL){
+            if(curr -> left != NULL){
+                if(prev != NULL){
+                    prev -> next = curr -> left;
+                }
+                else{
+                    leftmost = curr -> left;
+                }
+                prev = curr -> left;
+            }
+            if(curr -> right != NULL){   
+                if(prev != NULL){
+                    prev -> next = curr -> right;
+                }
+                else{
+                    leftmost = curr -> right;
+                }
+                prev = curr -> right;
+            }
+            curr = curr -> next;
+        }
+        curr = leftmost;
+        prev = NULL;
+        leftmost = NULL;
+    }
+}
