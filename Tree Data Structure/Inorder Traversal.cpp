0b0c421eb46c7af015ca2ca42a6fc11901c89f7b
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-04-06 13:34:48 +0530

    Update Inorder Traversal.cpp

--- Tree Data Structure/Inorder Traversal.cpp
+++ Tree Data Structure/Inorder Traversal.cpp
@@ -1,3 +1,4 @@
+//Recurive
 /**
  * Definition for binary tree
  * struct TreeNode {
@@ -24,4 +25,38 @@ vector<int> Solution::inorderTraversal(TreeNode* A) {
     solve(A,v);
     
     return v;
+}
+
+//Iterative
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+vector<int> Solution::inorderTraversal(TreeNode* A) {
+
+    stack<TreeNode*> st;
+    
+    while(A){
+        st.push(A);
+        A = A -> left;
+    }
+
+    vector<int> res;
+
+    while(!st.empty()){
+        TreeNode* curr = st.top();
+        st.pop();
+        res.push_back(curr -> val);
+        curr = curr -> right;
+        while(curr){
+            st.push(curr);
+            curr = curr -> left;
+        }
+    }
+    return res;
 }
