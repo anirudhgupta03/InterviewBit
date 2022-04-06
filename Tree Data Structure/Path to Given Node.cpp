82bce088617e8597692565a02a3650c68bdae65c
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-04-06 09:28:25 +0530

    Update Path to Given Node.cpp

--- Tree Data Structure/Path to Given Node.cpp
+++ Tree Data Structure/Path to Given Node.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 //Using Backtracking
 /**
  * Definition for binary tree
@@ -29,5 +30,43 @@ vector<int> Solution::solve(TreeNode* A, int B) {
     
     vector<int> path,temp;
     findPath(A, path,temp,B);
+    return path;
+}
+
+//Method - 2
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+bool findPath(TreeNode* root, vector<int> &path, int B){
+    
+    if(root == NULL){
+        return false;
+    }
+    
+    if(root -> val == B){
+        path.push_back(B);
+        return true;
+    }
+    
+    if(findPath(root->left,path,B) || findPath(root->right,path,B)){
+        path.push_back(root->val);
+        return true;
+    }
+    return false;
+}
+vector<int> Solution::solve(TreeNode* A, int B) {
+    
+    vector<int> path;
+    
+    findPath(A, path,B);
+    
+    reverse(path.begin(),path.end());
+    
     return path;
 }
