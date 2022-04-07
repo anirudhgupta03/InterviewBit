39f6206a425235e3f208d6f22a09b9e776fa4389
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-04-07 11:47:46 +0530

    Update Sum Root to Leaf Numbers.cpp

--- Tree Data Structure/Sum Root to Leaf Numbers.cpp
+++ Tree Data Structure/Sum Root to Leaf Numbers.cpp
@@ -32,3 +32,32 @@ int Solution::sumNumbers(TreeNode* A) {
     
     return ans%1003;
 }
+
+//Alter
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+int mod = 1e3 + 3;
+int sum;
+void solve(int num, TreeNode* A){
+    if(A == NULL){
+        return;
+    }
+    if(A -> left == NULL && A -> right == NULL){
+        sum = (sum + 10*num + A -> val)%mod;
+        return;
+    }
+    solve((10*num + A -> val)%mod, A -> left);
+    solve((10*num + A -> val)%mod, A -> right);
+}
+int Solution::sumNumbers(TreeNode* A) {
+    sum = 0;
+    solve(0, A);
+    return sum%mod;
+}
