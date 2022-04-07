2022a9d53a3ab99f229ccd9272b9e52327a29a73
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-04-07 13:15:04 +0530

    Update Path Sum.cpp

--- Tree Data Structure/Path Sum.cpp
+++ Tree Data Structure/Path Sum.cpp
@@ -28,3 +28,29 @@ int Solution::hasPathSum(TreeNode* A, int B) {
     }
     return 0;
 }
+
+//Alter
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+int Solution::hasPathSum(TreeNode* A, int B) {
+    if(A == NULL){
+        return 0;
+    }
+    if(A -> left == NULL && A -> right == NULL){
+        if(B - A -> val == 0){
+            return 1;
+        }
+        return 0;
+    }
+    int flag = hasPathSum(A -> left, B - A -> val);
+    if(flag) return 1;
+    flag = hasPathSum(A -> right, B - A -> val);
+    return flag;
+}
