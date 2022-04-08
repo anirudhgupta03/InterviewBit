7ab000de9c9fefdf57df19631f52240a59eb4bc5
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-04-08 09:59:52 +0530

    Update Flatten Binary Tree to Linked List.cpp

--- Tree Data Structure/Flatten Binary Tree to Linked List.cpp
+++ Tree Data Structure/Flatten Binary Tree to Linked List.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 //Ref: https://www.youtube.com/watch?v=SrTydnU2aj8
 /**
  * Definition for binary tree
@@ -36,4 +37,59 @@ TreeNode* Solution::flatten(TreeNode* A) {
     
     solve(A);
     return A;
+}
+
+//Method - 2
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ * 
+ * typedef struct TreeNode treenode;
+ * 
+ * treenode* treenode_new(int val) {
+ *     treenode* node = (treenode *) malloc(sizeof(treenode));
+ *     node->val = val;
+ *     node->left = NULL;
+ *     node->right = NULL;
+ *     return node;
+ * }
+ */
+/**
+ * @input A : Root pointer of the tree 
+ * 
+ * @Output root pointer of tree.
+ */
+treenode* solve(treenode* A){
+    if(A == NULL){
+        return NULL;
+    }
+    treenode* l = solve(A -> left);
+    treenode* r = solve(A -> right);
+
+    if(l && r){
+        treenode* temp1 = A -> left;
+        treenode* temp2 = A -> right;
+        A -> right = temp1;
+        l -> right = temp2;
+        A -> left = NULL;
+        return r;
+    }
+    if(l){
+        treenode* temp = A -> left; 
+        A -> left = NULL;
+        A -> right = temp;
+        return l;
+    }
+    if(r){
+        return r;
+    }
+    return A;
+}
+treenode* flatten(treenode* A) {
+    solve(A);
+    return A;    
 }
