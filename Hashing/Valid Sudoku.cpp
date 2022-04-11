624d409c88ba39f013c7d796cf2827d9d8df6cef
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-04-11 17:12:25 +0530

    Update Valid Sudoku.cpp

--- Hashing/Valid Sudoku.cpp
+++ Hashing/Valid Sudoku.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 int Solution::isValidSudoku(const vector<string> &A) {
     
     int temp2[A.size()][A.size()+1];
@@ -43,6 +44,61 @@ int Solution::isValidSudoku(const vector<string> &A) {
                 }
             }
         }
+    }
+    return 1;
+}
+
+//Method - 2
+int Solution::isValidSudoku(const vector<string> &A) {
+    
+    for(int j = 0; j < 9; j++){
+        vector<int> vis(10, 0);
+        for(int i = 0; i < 9; i++){
+            if(A[i][j] == '.') continue;
+            if(vis[A[i][j] - '0']){
+                return 0;
+            }
+            vis[A[i][j] - '0'] = 1;
+        }
+    }
+
+    for(int i = 0; i < 9; i++){
+        vector<int> vis(10,0);
+        for(int j = 0; j < 9; j++){
+            if(A[i][j] == '.') continue;
+            if(vis[A[i][j] - '0']){
+                return 0;
+            }
+            vis[A[i][j] - '0'] = 1;
+        }
+    }  
+    int i = 0, j = 0, count = 1, tcount = 0;
+    while(1){
+        if(i == 9 && j == 9){
+            break;
+        }
+        if(j == 9){
+            tcount = 1;
+            i = 3*count;
+            count++;
+            j = 0;
+        }
+        else{
+            tcount++;
+            i = 3*(count - 1);
+        }
+       
+        vector<int> vis(10, 0);
+        for(; i < count*3; i++){
+            for(int k = j; k < tcount*3; k++){
+                if(A[i][k] == '.') continue;
+                if(vis[A[i][k] - '0']){
+                    return 0;
+                }
+                vis[A[i][k] - '0'] = 1;
+            }
+        }
+        j = 3*tcount;
     }
     return 1;
 }
