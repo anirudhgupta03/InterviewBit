34b6f214ee10fdc9d423ccef4e1ef773191394fa
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-04-11 17:17:11 +0530

    Update Diffk II.cpp

--- Hashing/Diffk II.cpp
+++ Hashing/Diffk II.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 int Solution::diffPossible(const vector<int> &A, int B) {
     
     vector<int> temp = A;
@@ -14,6 +15,23 @@ int Solution::diffPossible(const vector<int> &A, int B) {
         else{
             umap[temp[i]] = 1;
         }
+    }
+    return 0;
+}
+
+//Method - 2
+int Solution::diffPossible(const vector<int> &A, int B) {
+    
+    vector<int> temp = A;
+    
+    unordered_map<int,int> umap;
+    
+    for(int i = 0; i < temp.size(); i++){
+        
+        if(umap.find(temp[i]-B) != umap.end() || umap.find(temp[i]+B) != umap.end()){
+            return 1;
+        }
+        umap[temp[i]] = 1;
     }
     return 0;
 }
