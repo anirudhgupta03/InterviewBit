0b8999439c5433ae959fb67cc5793e6f25d7f254
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-04-30 09:24:24 +0530

    Update Subarray with given XOR.cpp

--- Hashing/Subarray with given XOR.cpp
+++ Hashing/Subarray with given XOR.cpp
@@ -1,4 +1,5 @@
 //Similar to number of subarrays with sum k
+//Method - 1
 int Solution::solve(vector<int> &A, int B) {
     
     int currxor = 0;
@@ -20,6 +21,25 @@ int Solution::solve(vector<int> &A, int B) {
         }
         
         umap[currxor]++;
+    }
+    return count;
+}
+
+//Method - 2
+int Solution::solve(vector<int> &A, int B) {
+
+    int count = 0;
+    unordered_map<int,int> umap;
+
+    int XOR = 0;
+    umap[0] = 1;
+    
+    for(int i = 0; i < A.size(); i++){
+        XOR = XOR^A[i];
+        if(umap.find(XOR^B) != umap.end()){
+            count += umap[XOR^B];
+        }
+        umap[XOR]++;
     }
     return count;
 }
