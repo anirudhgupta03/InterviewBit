b89de68000c89e08811e8e012953e46129b29f5b
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-05-01 17:09:39 +0530

    Update Modular Expression.cpp

--- Backtracking/Modular Expression.cpp
+++ Backtracking/Modular Expression.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 int power(int A, int B, int C){
     
     if(B == 0){
@@ -22,4 +23,32 @@ int Solution::Mod(int A, int B, int C) {
         return (power(A,B,C)+C)%C;
     }
     return power(A,B,C)%C;
+}
+
+//Method - 2
+#define ll long long
+ll solve(ll A, ll B, ll C){
+    
+    if(A == 1){
+        return A % C;
+    }
+    if(B == 0){
+        return 1 % C;
+    }
+
+    if(B % 2 == 0){
+        ll temp = solve(A, B/2, C)%C;
+        return (temp*temp)%C;
+    }
+    ll temp = solve(A, (B - 1)/2, C);
+    
+    if(A < 0){
+        return ((((A*temp)%C)*temp)%C + C)%C;
+    }
+    else{
+        return (((A*temp)%C)*temp)%C;
+    }
+}
+int Solution::Mod(int A, int B, int C) {
+    return solve(A, B, C);
 }
