f354d04d67627dc3dea11aa02dd56d6a5e58e57a
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-05-08 15:59:40 +0530

    Update Combination Sum II.cpp

--- Backtracking/Combination Sum II.cpp
+++ Backtracking/Combination Sum II.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 void solve(int ind, vector<int> &A, int B, vector<int> &v, set<vector<int>> &res){
     
     if(B == 0){
@@ -35,5 +36,28 @@ vector<vector<int> > Solution::combinationSum(vector<int> &A, int B) {
         ans.push_back(*it);
     }
     
+    return ans;
+}
+
+//Method - 2
+//Ref: https://www.youtube.com/watch?v=G1fRTGRxXU8&t=1159s
+void solve(int ind, int B, vector<int> &A, vector<int> &res, vector<vector<int>> &ans){
+    if(B == 0){
+        ans.push_back(res);
+        return;
+    }
+    for(int i = ind; i < A.size(); i++){
+        if(i > ind && A[i] == A[i - 1]) continue;
+        if(A[i] > B) break;
+        res.push_back(A[i]);
+        solve(i + 1, B - A[i], A, res, ans);
+        res.pop_back();
+    }
+}
+vector<vector<int> > Solution::combinationSum(vector<int> &A, int B) {
+    sort(A.begin(), A.end());
+    vector<int> res;
+    vector<vector<int>> ans;
+    solve(0, B, A, res, ans);
     return ans;
 }
