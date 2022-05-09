03518a9c6c1cb13661e9367964ff0c7a650d6746
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-05-09 11:26:12 +0530

    Update Letter Phone.cpp

--- Backtracking/Letter Phone.cpp
+++ Backtracking/Letter Phone.cpp
@@ -1,34 +1,28 @@
-void solve(int ind, string A, vector<string> &v, string &temp, map<int,string> &m){
-    
-    if(temp.size() == A.size()){
-        v.push_back(temp);
+void solve(int ind, string &A, string &temp, vector<string> &res, unordered_map<char,string> &umap){
+    if(ind == A.size()){
+        res.push_back(temp);
         return;
     }
-    for(int j = 0; j < m[A[ind]-48].size(); j++){
-        temp.push_back(m[A[ind]-48][j]);
-        solve(ind+1,A,v,temp,m);
+    for(int i = 0; i < umap[A[ind]].size(); i++){
+        temp.push_back(umap[A[ind]][i]);
+        solve(ind + 1, A, temp, res, umap);
         temp.pop_back();
     }
 }
 vector<string> Solution::letterCombinations(string A) {
-    
-    map<int,string> m;
-    
-    m[0] = "0";
-    m[1] = "1";
-    m[2] = "abc";
-    m[3] = "def";
-    m[4] = "ghi";
-    m[5] = "jkl";
-    m[6] = "mno";
-    m[7] = "pqrs";
-    m[8] = "tuv";
-    m[9] = "wxyz";
-    
-    vector<string> v;
+    unordered_map<char,string> umap;
+    umap['0'] = "0";
+    umap['1'] = "1";
+    umap['2'] = "abc";
+    umap['3'] = "def";
+    umap['4'] = "ghi";
+    umap['5'] = "jkl";
+    umap['6'] = "mno";
+    umap['7'] = "pqrs";
+    umap['8'] = "tuv";
+    umap['9'] = "wxyz";
+    vector<string> res;
     string temp;
-    
-    solve(0,A,v,temp,m);
-    
-    return v;
+    solve(0, A, temp, res, umap);
+    return res;
 }
