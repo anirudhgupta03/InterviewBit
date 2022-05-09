bad2d6eecd9ebe95f3b1b7afa59c727607426c84
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-05-09 11:10:31 +0530

    Update Subsets II.cpp

--- Backtracking/Subsets II.cpp
+++ Backtracking/Subsets II.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 void solve(int ind, vector<int> &A, set<vector<int>> &s, vector<int> &p){
     
     if(ind == A.size()){
@@ -37,4 +38,26 @@ vector<vector<int> > Solution::subsetsWithDup(vector<int> &A) {
     sort(v.begin(),v.end());
     
     return v;
+}
+
+//Method - 2
+//Optimal
+//Ref: https://www.youtube.com/watch?v=RIn3gOkbhQE
+void solve(int ind, vector<int> &A, vector<int> &temp, vector<vector<int>> &res){
+
+    res.push_back(temp);
+    for(int i = ind; i < A.size(); i++){
+        if(i > ind && A[i] == A[i - 1]) continue;
+        temp.push_back(A[i]);
+        solve(i + 1, A, temp, res);
+        temp.pop_back();
+    }
+}
+vector<vector<int> > Solution::subsetsWithDup(vector<int> &A) {
+
+      vector<vector<int>> res;
+      vector<int> temp;
+      sort(A.begin(), A.end());
+      solve(0, A, temp, res);
+      return res;
 }
