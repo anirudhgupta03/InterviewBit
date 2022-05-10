45beb8a88b80d7bf0b1f5f9ad2af5e279fa5cbce
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-05-10 15:08:07 +0530

    Update MAXSPPROD.cpp

--- Stacks and Queues/MAXSPPROD.cpp
+++ Stacks and Queues/MAXSPPROD.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 #define ll long long
 int Solution::maxSpecialProduct(vector<int> &A) {
     
@@ -70,4 +71,69 @@ int Solution::maxSpecialProduct(vector<int> &A) {
         }
     }
     return ans%1000000007;
+}
+
+//Method - 2
+#define ll long long
+#define mod 1000000007
+int Solution::maxSpecialProduct(vector<int> &A) {
+
+    int n = A.size();
+
+    ll maxProd = 0;
+
+    vector<ll> ngl(n), ngr(n);
+
+    stack<ll> st;
+    st.push(0);
+
+    ngl[0] = -1;
+    for(int i = 1; i < n; i++){
+        if(A[i] < A[st.top()]){
+            ngl[i] = st.top();
+        }  
+        else{
+            while(!st.empty() && A[i] >= A[st.top()]){
+                st.pop();
+            }
+
+            if(st.empty()){
+                ngl[i] = -1;
+            }
+            else{
+                ngl[i] = st.top();
+            }
+        }
+        st.push(i);
+    }
+
+    while(!st.empty()) st.pop();
+    st.push(n - 1);
+    ngr[n - 1] = -1;
+
+    for(int i = n - 2; i >= 0; i--){
+        if(A[i] < A[st.top()]){
+            ngr[i] = st.top();
+        }  
+        else{
+            while(!st.empty() && A[i] >= A[st.top()]){
+                st.pop();
+            }
+
+            if(st.empty()){
+                ngr[i] = -1;
+            }
+            else{
+                ngr[i] = st.top();
+            }
+        }
+        st.push(i);
+    }
+
+    for(int i = 1; i < n - 1; i++){
+        if(ngl[i] != -1 && ngr[i]  != -1){
+            maxProd = max(maxProd, ngl[i]*ngr[i]);
+        }
+    }
+    return maxProd%mod;
 }
