07399c5cac73d132067ceb55ed5b362ccdf81134
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-05-10 15:40:02 +0530

    Update Largest Rectangle in Histogram.cpp

--- Stacks and Queues/Largest Rectangle in Histogram.cpp
+++ Stacks and Queues/Largest Rectangle in Histogram.cpp
@@ -1,4 +1,5 @@
 //Use NSL and NSR approach
+//Method - 1
 int Solution::largestRectangleArea(vector<int> &A) {
     
     int n = A.size();
@@ -69,4 +70,61 @@ int Solution::largestRectangleArea(vector<int> &A) {
     }
     
     return ans;
+}
+
+//Method - 2
+int Solution::largestRectangleArea(vector<int> &A) {
+
+    int n = A.size();
+    vector<int> nsl(n), nsr(n);
+
+    stack<int> st;
+    st.push(0);
+    nsl[0] = -1;
+
+    for(int i = 1; i < n; i++){
+        if(A[i] > A[st.top()]){
+            nsl[i] = st.top();
+        }
+        else{
+            while(!st.empty() && A[i] <= A[st.top()]){
+                st.pop();
+            }
+            if(st.empty()){
+                nsl[i] = -1;
+            }
+            else{
+                nsl[i] = st.top();
+            }
+        }
+        st.push(i);
+    }
+
+    while(!st.empty()) st.pop();
+    nsr[n - 1] = n;
+    st.push(n - 1);
+
+    for(int i = n - 2; i >= 0; i--){
+        if(A[i] > A[st.top()]){
+            nsr[i] = st.top();
+        }
+        else{
+            while(!st.empty() && A[i] <= A[st.top()]){
+                st.pop();
+            }
+            if(st.empty()){
+                nsr[i] = n;
+            }
+            else{
+                nsr[i] = st.top();
+            }
+        }
+        st.push(i);
+    }
+
+    int maxArea = INT_MIN;
+    for(int i = 0; i < n; i++){
+        maxArea = max(maxArea, A[i]*(nsr[i] - nsl[i] - 1));
+    }
+    return maxArea;
 }
