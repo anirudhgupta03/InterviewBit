cb82a763a53bf81923beac6ab8c313a1ea66fcdd
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-05-17 09:25:06 +0530

    Update Number of 1 Bits.cpp

--- Bit Manipulation/Number of 1 Bits.cpp
+++ Bit Manipulation/Number of 1 Bits.cpp
@@ -1,3 +1,6 @@
+//Method - 1
+//TC - O(set bits)
+//Flip the least significant bit
 int Solution::numSetBits(unsigned int A) {
     
     int count = 0;
@@ -5,6 +8,20 @@ int Solution::numSetBits(unsigned int A) {
     while(A){
         A = A & (A-1);
         count++;
+    }
+    return count;
+}
+
+//Method - 2
+//TC - O(32)
+//SC - O(1)
+int Solution::numSetBits(unsigned int A) {
+    int count = 0;
+    while(A){
+        if(A % 2 == 1){
+            count++;
+        }
+        A /= 2;
     }
     return count;
 }
