7f74eea5487be458f2f35b4d5c46cc11b6bbd27c
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-05-17 18:08:22 +0530

    Update Count Total Set Bits.cpp

--- Bit Manipulation/Count Total Set Bits.cpp
+++ Bit Manipulation/Count Total Set Bits.cpp
@@ -1,17 +1,19 @@
 //Ref: https://www.youtube.com/watch?v=g6OxU-hRGtY
+#define mod 1000000007
+#define ll long long
 int Solution::solve(int A) {
-    
     if(A == 0){
         return 0;
     }
-    
-    long long p = 0, ans = 0;
-    
-    while(pow(2,p) <= A){
+
+    ll p = 0;
+    while((1 << p) <= A){
         p++;
     }
     p--;
-    ans += pow(2,p-1)*p + (A - pow(2,p) + 1) + solve(A - pow(2,p));
-    ans %= 1000000007;
-    return ans;
+
+    ll bitstill2x = (1 << (p - 1))*p;
+    ll msb2xton = A - (1 << p) + 1;
+    ll ans = bitstill2x + msb2xton + solve(A - (1 << p));
+    return ans%mod;
 }
