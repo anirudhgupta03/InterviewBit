bf5569e74b26d9c9ad41563a0290e488b03a5652
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-05-18 17:50:54 +0530

    Update Implement StrStr.cpp

--- Strings/Implement StrStr.cpp
+++ Strings/Implement StrStr.cpp
@@ -39,3 +39,61 @@ int Solution::strStr(const string A, const string B) {
     }
     return -1;
 }
+
+//KMP String Matching Algo
+//TC - O(n)
+//SC - O(n)
+int Solution::strStr(const string A, const string B) {
+
+    if(A.size() == 0 || B.size() == 0){
+        return -1;
+    }
+    if(B.size() > A.size()){
+        return -1;
+    }
+    int n = B.size();
+
+    vector<int> lps(n);
+
+    lps[0] = 0;
+
+    int j = 0, i = 1;
+
+    while(i < n){
+        if(B[i] == B[j]){
+            lps[i] = j + 1;
+            i++;
+            j++;
+        }
+        else{
+            if(j != 0) {
+                j = lps[j - 1];
+            }
+            else{
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+    
+    i = 0, j = 0;
+
+    while(i < A.size() && j < B.size()){
+        if(A[i] == B[j]){
+            i++;
+            j++;
+        }
+        else{
+            if(j != 0){
+                j = lps[j - 1];
+            }
+            else{
+                i++;
+            }
+        }
+    }
+    if(j == B.size()){
+        return i - B.size();
+    }
+    return -1;
+}
