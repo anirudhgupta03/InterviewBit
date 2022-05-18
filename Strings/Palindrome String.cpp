450d956cdb0e36f8bacea88004b82640864a700f
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-05-18 11:37:05 +0530

    Update Palindrome String.cpp

--- Strings/Palindrome String.cpp
+++ Strings/Palindrome String.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 int Solution::isPalindrome(string A) {
     
     string p;
@@ -19,6 +20,30 @@ int Solution::isPalindrome(string A) {
         }
         l++;
         r--;
+    }
+    return 1;
+}
+
+//Method - 2
+int Solution::isPalindrome(string A) {
+
+    int lo = 0, hi = A.size() - 1;
+
+    while(lo < hi){
+        
+        while(lo < hi && !(isalpha(A[lo]) || A[lo] >= '0' && A[lo] <= '9')) lo++;
+        while(lo < hi && !(isalpha(A[hi]) || A[hi] >= '0' && A[hi] <= '9')) hi--;
+
+        if(isalpha(A[lo]) && isalpha(A[hi])){
+            if(tolower(A[lo]) != tolower(A[hi])){
+                return 0;
+            }
+        }
+        else if(A[lo] != A[hi]){
+            return 0;
+        }
+        lo++;
+        hi--;
     }
     return 1;
 }
