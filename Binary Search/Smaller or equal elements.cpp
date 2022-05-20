289aa4439d650f5b035e5b563ec1f06b0613fcf9
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-05-20 22:34:32 +0530

    Update Smaller or equal elements.cpp

--- Binary Search/Smaller or equal elements.cpp
+++ Binary Search/Smaller or equal elements.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 int Solution::solve(vector<int> &A, int B) {
     
     int l = 0, r = A.size() - 1, ind = -1;
@@ -14,4 +15,24 @@ int Solution::solve(vector<int> &A, int B) {
         }
     }
     return ind+1;
+}
+
+//Method - 2
+int Solution::solve(vector<int> &A, int B) {
+
+    int lo = 0, hi = A.size() - 1, ans = A.size();
+
+    while(lo <= hi){
+
+        int mid = (lo + hi)/2;
+
+        if(A[mid] > B){
+            ans = mid;
+            hi = mid - 1;
+        }
+        else{
+            lo = mid + 1;
+        }
+    }
+    return ans;
 }
