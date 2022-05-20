b8c9da2972b2f099f885a80903c9714a6a68c0ab
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-05-20 23:54:50 +0530

    Update Matrix Median.cpp

--- Binary Search/Matrix Median.cpp
+++ Binary Search/Matrix Median.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 //Time Complexity - O(32*NlogM)
 //Space Complexity - O(1)
 int Solution::findMedian(vector<vector<int> > &A) {
@@ -33,4 +34,52 @@ int Solution::findMedian(vector<vector<int> > &A) {
         }
     }
     return median;
+}
+
+//Method - 2
+int elementsLessThanOrEqualToMedian(int median, vector<vector<int>> &A){
+    int count = 0, n = A.size(), m = A[0].size();
+    
+    for(int i = 0; i < n; i++){
+        
+        int ind = -1, lo = 0, hi = m - 1;
+        while(lo <= hi){
+            int mid = (lo + hi)/2;
+            if(A[i][mid] <= median){
+                ind = mid;
+                lo = mid + 1;
+            }
+            else{
+                hi = mid - 1;
+            }
+        }
+        count += ind + 1;
+    }
+    return count;
+}
+int Solution::findMedian(vector<vector<int> > &A) {
+
+    int n = A.size(), m = A[0].size();
+
+    int lo = INT_MAX, hi = INT_MIN;
+
+    for(int i = 0; i < n; i++){
+        lo = min(lo, A[i][0]);
+        hi = max(hi, A[i][m - 1]);
+    }
+
+    int count = (m*n + 1)/2, ans;
+
+    while(lo <= hi){
+        int mid = (lo + hi)/2;
+        
+        if(elementsLessThanOrEqualToMedian(mid, A) >= count){
+            ans = mid;
+            hi = mid - 1;
+        }
+        else{
+            lo = mid + 1;
+        }
+    }
+    return ans;
 }
