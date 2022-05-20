092387283d53bbe4b99ba2867b9576d00008bcb8
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-05-20 22:22:01 +0530

    Update Search in Bitonic Array!.cpp

--- Binary Search/Search in Bitonic Array!.cpp
+++ Binary Search/Search in Bitonic Array!.cpp
@@ -1,5 +1,66 @@
 //Method - 1
 int Solution::solve(vector<int> &A, int B) {
+    
+    int n = A.size();
+    
+    int lo = 0, hi = A.size() - 1;
+    
+    int bitonicInd;
+    
+    while(lo <= hi){
+        int mid = (lo + hi)/2;
+        
+        if(mid != 0 && mid != n-1 && A[mid] > A[mid+1] && A[mid] > A[mid-1]){
+           bitonicInd = mid;
+           break;
+        }
+        else if((mid == 0 || mid != 0 && A[mid] > A[mid-1]){
+            lo = mid + 1;
+        }
+        else if(mid == n -  || mid != n - 1 && A[mid] < A[mid-1]){
+            hi = mid - 1;
+        }
+    }
+    
+    lo = 0, hi = bitonicInd;
+    
+    while(lo <= hi){
+        
+        int mid = (lo + hi)/2;
+        
+        if(A[mid] == B){
+            return mid;
+        }
+        else if(A[mid] < B){
+            lo = mid + 1;
+        }
+        else{
+            hi = mid - 1;
+        }
+    }
+    
+    lo = bitonicInd + 1, hi = n - 1;
+    
+    while(lo <= hi){
+        
+        int mid = (lo + hi)/2;
+        
+        if(A[mid] == B){
+            return mid;
+        }
+        else if(A[mid] < B){
+            hi = mid - 1;
+        }
+        else{
+            lo = mid + 1;
+        }
+    }
+    return -1;
+}
+
+//Method - 2
+//Optimal Method
+int Solution::solve(vector<int> &A, int B) {
 
     int n = A.size();
 
@@ -46,66 +107,6 @@ int Solution::solve(vector<int> &A, int B) {
     while(lo <= hi){
         int mid = (lo + hi)/2;
 
-        if(A[mid] == B){
-            return mid;
-        }
-        else if(A[mid] < B){
-            hi = mid - 1;
-        }
-        else{
-            lo = mid + 1;
-        }
-    }
-    return -1;
-}
-
-//Method - 2
-int Solution::solve(vector<int> &A, int B) {
-    
-    int n = A.size();
-    
-    int lo = 0, hi = A.size() - 1;
-    
-    int bitonicInd;
-    
-    while(lo <= hi){
-        int mid = (lo + hi)/2;
-        
-        if(mid != 0 && mid != n-1 && A[mid] > A[mid+1] && A[mid] > A[mid-1]){
-           bitonicInd = mid;
-           break;
-        }
-        else if((mid == 0 || mid != 0 && A[mid] > A[mid-1]){
-            lo = mid + 1;
-        }
-        else if(mid == n -  || mid != n - 1 && A[mid] < A[mid-1]){
-            hi = mid - 1;
-        }
-    }
-    
-    lo = 0, hi = bitonicInd;
-    
-    while(lo <= hi){
-        
-        int mid = (lo + hi)/2;
-        
-        if(A[mid] == B){
-            return mid;
-        }
-        else if(A[mid] < B){
-            lo = mid + 1;
-        }
-        else{
-            hi = mid - 1;
-        }
-    }
-    
-    lo = bitonicInd + 1, hi = n - 1;
-    
-    while(lo <= hi){
-        
-        int mid = (lo + hi)/2;
-        
         if(A[mid] == B){
             return mid;
         }
