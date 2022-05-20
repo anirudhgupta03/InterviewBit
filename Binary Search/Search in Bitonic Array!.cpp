d01392f334045f9083c5c2bfd87f99c0cda7e7ec
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-05-20 22:10:05 +0530

    Update Search in Bitonic Array!.cpp

--- Binary Search/Search in Bitonic Array!.cpp
+++ Binary Search/Search in Bitonic Array!.cpp
@@ -1,56 +1,59 @@
 //Method - 1
 int Solution::solve(vector<int> &A, int B) {
-    
-    int l = 0, r = A.size() - 1 ,ind;
-    
-    while(l<=r){
-        
-        int mid = (l+r)/2;
-        
-        if(A[mid] > A[mid-1] && A[mid] > A[mid+1]){
-                ind = mid;
-                break;
-        }
-        else if(A[mid] < A[mid+1]){
-            l = mid + 1;
-        }
-        else if( A[mid] < A[mid-1]){
-            r = mid - 1;
-        }
-        
-    }
-    
-    l = 0, r= ind - 1;
-    
-    while(l<=r){
-        
-        int mid = (l+r)/2;
-        
-        if(A[mid] < B){
-            l =  mid + 1;
-        }
-        else if(A[mid] > B){
-            r = mid - 1;
+
+    int n = A.size();
+
+    int lo = 0, hi = n - 1, pivot;
+
+    while(lo <= hi){
+        int mid = (lo + hi)/2;
+
+        if(A[mid] > A[mid - 1] && A[mid] > A[mid + 1]){
+            pivot = mid;
+            break;
         }
         else{
-            return mid;
+            if(A[mid] < A[mid + 1]){
+                lo = mid + 1;
+            }
+            else{
+                hi = mid - 1;
+            }
         }
     }
     
-    l = ind+1, r= A.size()-1;
-    
-    while(l<=r){
-        
-        int mid = (l+r)/2;
-        
-        if(A[mid] > B){
-            l =  mid + 1;
+    if(A[pivot] == B){
+        return pivot;
+    }
+    lo = 0, hi = pivot - 1;
+
+    while(lo <= hi){
+        int mid = (lo + hi)/2;
+
+        if(A[mid] == B){
+            return mid;
         }
         else if(A[mid] < B){
-            r = mid - 1;
+            lo = mid + 1;
         }
         else{
+            hi = mid - 1;
+        }
+    }
+
+    lo = pivot + 1, hi = n - 1;
+
+    while(lo <= hi){
+        int mid = (lo + hi)/2;
+
+        if(A[mid] == B){
             return mid;
+        }
+        else if(A[mid] < B){
+            hi = mid - 1;
+        }
+        else{
+            lo = mid + 1;
         }
     }
     return -1;
