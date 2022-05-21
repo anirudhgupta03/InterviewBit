dc31501c6cdc633a6749d5cf6753437cc003e4e9
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-05-21 14:41:15 +0530

    Update Implement Power Function.cpp

--- Binary Search/Implement Power Function.cpp
+++ Binary Search/Implement Power Function.cpp
@@ -1,27 +1,20 @@
 //Method - 1
 #define ll long long
-int power(int base, int expo, int mod){
-    
-    if(expo == 0){
-        return 1;
-    }
-    if(expo == 1){
-        return (base + mod) % mod;
-    }
-    
-    if(expo % 2 == 0){
-        ll p = power(base,expo/2,mod);
-        return (p*p)%mod;
-    }
-    else{
-        ll p = power(base,(expo-1)/2,mod);
-        
-        return ((((p*p)%mod)*base)%mod + mod)%mod;    //To handle negative base we add mod
-    }
+ll myPower(ll x, ll n, ll d){
+
+    if(n == 0) return 1 % d;
+    if(n == 1) return (x % d + d) % d;
+
+    ll temp;
+    if(n % 2 == 0){
+        temp = myPower(x, n/2, d);
+        return (temp*temp)%d;
+    } 
+    temp = myPower(x, (n - 1)/2, d);
+    return ((((x*temp)%d)*temp)%d + d)%d;
 }
 int Solution::pow(int x, int n, int d) {
-    
-    return power(x,n,d)%d;
+    return myPower(x, n, d);
 }
 
 //Method - 2
