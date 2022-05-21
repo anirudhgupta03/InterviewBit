7ef9a97968fb599dac31444e89255da26d298ed8
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-05-21 22:17:58 +0530

    Update Prime Numbers.cpp

--- Math/Prime Numbers.cpp
+++ Math/Prime Numbers.cpp
@@ -1,13 +1,11 @@
 vector<int> Solution::sieve(int A) {
 
     bool primes[A+1];
-    
     memset(primes,true,sizeof(primes));
+    primes[1] = false;
     
     for(int p = 2; p*p <= A; p++){
-        
-        if(primes[p]==true){
-            
+        if(primes[p]==true){    
             for(int i= p*p ;i <= A;i+=p){
                 primes[p]=false;
             }
