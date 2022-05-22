7d2e52ec4453dff068b49ee09db7fe1a52c80f13
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-05-22 15:46:26 +0530

    Update Power of Two Integers.cpp

--- Math/Power of Two Integers.cpp
+++ Math/Power of Two Integers.cpp
@@ -1,3 +1,24 @@
+//Method - 1
+int Solution::isPower(int A) {
+
+    if(A == 1) return 1;
+
+    for(int i = 2; i * i <= A; i++){
+
+        if(A % i == 0){
+            int B = A;
+            while(B % i == 0){
+                B /= i;
+            }
+            if(B == 1){
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+//Method - 2
 int pw(int base, int exponent){
     
     if(exponent == 0){
@@ -28,5 +49,25 @@ int Solution::isPower(int A) {
             return 1;
         }
     }
+    return 0;
+}
+
+//Method - 3
+int Solution::isPower(int A) {
+    if(A == 1) return 1;
+    int gcd = 0;
+    
+    for(int i = 2; i * i <= A; i++){
+        if(A % i == 0){
+            int count = 0;
+            while(A % i == 0){
+                A /= i;
+                count++;
+            }
+            gcd = __gcd(gcd, count);
+        }
+    }
+    if(A != 1) return 0;
+    if(gcd != 1) return 1;
     return 0;
 }
