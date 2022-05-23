1b6c9b217aede4caa3ac784e60906390cdf49fbd
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-05-23 16:18:45 +0530

    Update Palindrome Integer.cpp

--- Math/Palindrome Integer.cpp
+++ Math/Palindrome Integer.cpp
@@ -1,20 +1,49 @@
+//Method - 1
 int Solution::isPalindrome(int A) {
-    
-    if(A<0){
-        return 0;
+
+    if(A >= 0 && A <= 9) return 1;
+
+    if(A < 0) return 0;
+
+    int len = 0, temp = A;
+
+    while(temp){
+        len++;
+        temp /= 10;
     }
-    
-    string s;
-    s= to_string(A);
-    int  i = 0, j = s.size()-1;
-    
-    while(i < j){
-        if(s[i] != s[j]){
-            return 0;
+
+    int lo = pow(10, len - 1);
+    int hi = 10;
+
+    int temp1 = A, temp2 = A;
+
+    len /= 2;
+
+    while(len--){
+        if(temp1 / lo != temp2 % hi){
+            return false;
         }
-        i++;
-        j--;
+        temp1 = temp1 % lo;
+        temp2 = temp2 / hi;
+        lo /= 10;
     }
+    return true;
+}
+
+//Method - 2
+int Solution::isPalindrome(int A) {
+
+    if(A < 0) return 0;
     
-    return 1;
+    int B = 0, temp = A;
+
+    while(temp){
+        B = 10*B + temp % 10;
+        temp /= 10;
+    }
+
+    if(A == B){
+        return 1;
+    }
+    return 0;
 }
