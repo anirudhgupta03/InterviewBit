e93d06985e455fbeaf6e32212a2620aaf5a12654
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-05-24 20:14:17 +0530

    Create Leap Year.cpp

---
+++ Math/Leap Year.cpp
@@ -0,0 +1,12 @@
+int Solution::solve(int A) {
+
+    if(A % 100 == 0){
+        if(A % 400 == 0){
+            return 1;
+        }
+    }
+    else if(A % 4 == 0){
+        return 1;
+    }
+    return 0;
+}
