c6888e8a231403e68b5031f864759c15523f3ec7
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-05-25 23:27:45 +0530

    Update Pick from both sides!.cpp

--- Arrays/Pick from both sides!.cpp
+++ Arrays/Pick from both sides!.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 int Solution::solve(vector<int> &A, int B) {
     
     int sum = 0;
@@ -19,4 +20,24 @@ int Solution::solve(vector<int> &A, int B) {
         r--;
     }
     return sum;
+}
+
+//Method - 2
+int Solution::solve(vector<int> &A, int B) {
+
+    int n = A.size();
+
+    int sum = 0, maxSum = INT_MIN;
+
+    for(int i = 0; i < B; i++){
+        sum += A[i];
+    }
+    maxSum = max(maxSum, sum);
+
+    for(int i = n - 1; i >= n - B; i--){
+        sum -= A[B - (n - i)];
+        sum += A[i];
+        maxSum = max(maxSum, sum);
+    }
+    return maxSum;
 }
