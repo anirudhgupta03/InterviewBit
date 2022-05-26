d9e7b7bfb6e3d0b5ea5e089cfa44d50f40cb7928
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-05-26 18:05:53 +0530

    Update Sort array with squares!.cpp

--- Arrays/Sort array with squares!.cpp
+++ Arrays/Sort array with squares!.cpp
@@ -1,3 +1,48 @@
+//Method - 1
+//TC - O(N)
+//SC - O(N)
+vector<int> Solution::solve(vector<int> &A) {
+
+    vector<int> pos, neg;
+
+    for(int i = 0; i < A.size(); i++){
+        if(A[i] < 0){
+            neg.push_back(A[i]*A[i]);
+        }
+        else{
+            pos.push_back(A[i]*A[i]);
+        }
+    }
+
+    vector<int> res;
+    
+    int ptr1 = 0, ptr2 = neg.size() - 1;
+
+    while(ptr1 < pos.size() && ptr2 >= 0){
+        if(pos[ptr1] < neg[ptr2]){
+            res.push_back(pos[ptr1]);
+            ptr1++;
+        }
+        else{
+            res.push_back(neg[ptr2]);
+            ptr2--;
+        }
+    }
+    while(ptr1 < pos.size()){
+        res.push_back(pos[ptr1]);
+        ptr1++;
+    }
+    while(ptr2 >= 0){
+        res.push_back(neg[ptr2]);
+        ptr2--;
+    }
+    return res;
+}
+
+
+//Method - 2
+//TC - O(N)
+//SC - O(1)
 vector<int> Solution::solve(vector<int> &A) {
     
     vector<int> ans(A.size());
