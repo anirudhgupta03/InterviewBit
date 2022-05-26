b352c8148933b834b00e3ac518c28fb720436199
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-05-26 15:28:30 +0530

    Update Spiral Order Matrix II.cpp

--- Arrays/Spiral Order Matrix II.cpp
+++ Arrays/Spiral Order Matrix II.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 vector<vector<int> > Solution::generateMatrix(int A) {
     
     int spiral[A][A];
@@ -58,4 +59,48 @@ vector<vector<int> > Solution::generateMatrix(int A) {
     }
     
     return v;
+}
+
+//Method - 2
+vector<vector<int> > Solution::generateMatrix(int A) {
+
+    vector<vector<int>> res(A, vector<int>(A));
+
+    int count = 1;
+
+    int top = 0, down = A - 1, left = 0, right = A - 1, dir = 0;
+
+    while(top <= down && left <= right){
+
+        if(dir == 0){
+            for(int i = left; i <= right; i++){
+                res[top][i] = count;
+                count++;
+            }
+            top++;
+        }
+        else if(dir == 1){
+            for(int i = top; i <= down; i++){
+                res[i][right] = count;
+                count++;
+            }
+            right--;
+        }
+        else if(dir == 2){
+            for(int i = right; i >= left; i--){
+                res[down][i] = count;
+                count++;
+            }
+            down--;
+        }
+        else if(dir == 3){
+            for(int i = down; i >= top; i--){
+                res[i][left] = count;
+                count++;
+            }
+            left++;
+        }
+        dir = (dir + 1) % 4;
+    }
+    return res;
 }
