d09bb7be68a27a362efbd2b04254169f98d06c0e
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-05-28 00:09:20 +0530

    Update Triplets with Sum between given range.cpp

--- Arrays/Triplets with Sum between given range.cpp
+++ Arrays/Triplets with Sum between given range.cpp
@@ -1,48 +1,26 @@
 int Solution::solve(vector<string> &A) {
+
+    int n = A.size();
+
+    if(n<3) return 0;
     
-    vector<float> v;
-    
-    for(int i = 0; i < A.size(); i++){
-        v.push_back(stof(A[i]));
-    }
-    
-    float a = v[0], b = v[1], c = v[2];
-    
-    for(int i = 3; i < v.size(); i++){
+    sort(A.begin(), A.end());
+
+    int l = 0, r = n-1;
+
+    while(l < r - 1){
         
-        if(a+b+c > 1 && a+b+c <2){
+        float s = stof(A[l]) + stof(A[l+1]) + stof(A[r]);
+        
+        if(s >= 2){
+            r = r - 1;
+        }
+        else if(s <= 1){
+            l = l + 1;
+        }
+        else{
             return 1;
         }
-        
-        else if(a+b+c >= 2){
-            
-            if(a >b && a>c){
-                a = v[i];
-            }
-            else if(b > a && b >c){
-                b = v[i];
-            }
-            else if(c >a && c>b){
-                c = v[i];
-            }
-        }
-        
-        else if(a+b+c <= 1){
-            
-            if(a < b && a<c){
-                a = v[i];
-            }
-            else if(b < c && b < a){
-                b = v[i];
-            }
-            else if(c<a && c < b){
-                c = v[i];
-            }
-        }
     }
-    
-    if(a+b+c > 1 && a+b+c < 2)
-        return 1;
-        
-        return 0;
+    return 0;
 }
