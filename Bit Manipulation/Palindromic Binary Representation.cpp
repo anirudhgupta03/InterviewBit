d564834ffee8ce3f50dbf8b966327acdea91046c
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-21 19:01:52 +0530

    Update Palindromic Binary Representation.cpp

--- Bit Manipulation/Palindromic Binary Representation.cpp
+++ Bit Manipulation/Palindromic Binary Representation.cpp
@@ -1,3 +1,55 @@
+//Method - 1
+int Solution::solve(int A) {
+    
+    if(A == 1){
+        return 1;
+    }
+    int len = 0, count = 1, sum = 0;
+    
+    while(1){
+        len++;
+        sum += count;
+        if(A <= sum){
+            break;
+        }
+        len++;
+        sum += count;
+        if(A <= sum){
+            break;
+        }
+        count *= 2;
+    }
+
+    int elementsInGroup = (1 << ((len - 1)/2));
+    int startOfGroup = sum - elementsInGroup + 1;
+    
+    int diff = A - startOfGroup;
+    
+    int res = 1;
+    res = (res << ((len - 1)/2));
+    res += diff;
+    
+    if(len % 2 != 0){
+        diff >>= 1;   
+    }
+    
+    res <<= (len/2);
+    int revdiff = 0, ptr = 1;
+    
+    while(ptr < len/2){
+        revdiff <<= 1;
+        revdiff |= (diff & 1);
+        diff >>= 1;
+        ptr++;
+    }
+    revdiff <<= 1;
+    revdiff += 1;
+    
+    res += revdiff;
+    return res;
+}
+
+//Method - 2
 //Ref: https://www.youtube.com/watch?v=QYoWR8hDCyQ
 //Very Tough
 int getReverse(int n){
