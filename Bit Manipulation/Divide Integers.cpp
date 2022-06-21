2769fc923e3454b2137e4d6a5c3d9c8a8f9c1fc0
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-21 15:25:03 +0530

    Update Divide Integers.cpp

--- Bit Manipulation/Divide Integers.cpp
+++ Bit Manipulation/Divide Integers.cpp
@@ -1,5 +1,7 @@
 //Ref: https://www.youtube.com/watch?v=X2Ko1Jt9bL4
 //Ref: https://www.youtube.com/watch?v=bdxJHWIyyqI&t=920s
+//Method - 1
+//Optimal
 #define ll long long
 int Solution::divide(int A, int B) {
     
@@ -29,4 +31,42 @@ int Solution::divide(int A, int B) {
     }
     
     return q;
+}
+
+//Method - 2
+#define ll long long
+int Solution::divide(int A, int B) {
+    
+    ll a = A;
+    ll b = B;
+    
+    int sign = 1;
+    
+    if(a < 0 && b > 0 || a > 0 && b < 0){
+        sign = -1;
+    }
+    
+    a = abs(a);
+    b = abs(b);
+    
+    ll res = 0;
+    
+    while(a >= b){
+        int ind = 32;
+        for(int i = 0; i < 32; i++){
+            if((b << i) > a){
+                ind = i;
+                break;
+            }
+        }
+        ind--;
+        res += (1LL << ind);
+        a -= (b << ind);
+    }
+    if(sign == -1) res = -res;
+    
+    if(res < INT_MIN || res > INT_MAX){
+        return INT_MAX;
+    }
+    return res;
 }
