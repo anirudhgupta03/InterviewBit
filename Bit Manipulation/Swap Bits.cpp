c26eba6d00f0c90c67c6fea221fb12ee348a2c7a
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-22 21:45:52 +0530

    Update Swap Bits.cpp

--- Bit Manipulation/Swap Bits.cpp
+++ Bit Manipulation/Swap Bits.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 int Solution::solve(int A, int B, int C) {
 
     B--;
@@ -5,5 +6,20 @@ int Solution::solve(int A, int B, int C) {
     int f1 = (A >> B) & 1;
     int f2 = (A >> C) & 1;
     A += (f1 << C) + (f2 << B) - (f1 << B) - (f2 << C);
+    return A;
+}
+
+//Method - 2
+int Solution::solve(int A, int B, int C) {
+    
+    int bitAtB = (A & (1 << (B - 1))) == 0 ? 0 : 1;
+    int bitAtC = (A & (1 << (C - 1))) == 0 ? 0 : 1;
+    
+    if(bitAtB == bitAtC){
+        return A;
+    }
+    
+    A = (A ^ (1 << (B - 1)));
+    A = (A ^ (1 << (C - 1)));
     return A;
 }
