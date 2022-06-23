ce63e0ea8356d4b0994b2ba4b8c477cb81cbf767
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-23 14:30:48 +0530

    Update Bulbs.cpp

--- Greedy Algorithm/Bulbs.cpp
+++ Greedy Algorithm/Bulbs.cpp
@@ -1,27 +1,10 @@
 //Time Complexity - O(N)
 //Space Complexity - O(1)
 int Solution::bulbs(vector<int> &A) {
-    
-    int n = A.size();
-    
-    if(n == 0){
-        return 0;
-    }
-    
     int count = 0;
-    
     for(int i = 0; i < A.size(); i++){
-        
-        if(count % 2 == 0){
-            if(A[i] == 0){
-                count++;
-            }
-        }
-        else{
-            A[i] = 1 - A[i];
-            if(A[i] == 0){
-                count++;
-            }
+        if(count % 2 == 0 && A[i] == 0 || count % 2 != 0 && A[i] == 1){
+            count++;
         }
     }
     return count;
