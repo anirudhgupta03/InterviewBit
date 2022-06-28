7b3391c5b28bfe159c24ff217e11f37d5be6630b
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-28 14:20:41 +0530

    Update Last Node in a Complete Binary Tree.cpp

--- Tree Data Structure/Last Node in a Complete Binary Tree.cpp
+++ Tree Data Structure/Last Node in a Complete Binary Tree.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 /**
  * Definition for binary tree
  * struct TreeNode {
@@ -22,4 +23,40 @@ int Solution::lastNode(TreeNode* A) {
         }
     }
     return ele;
+}
+
+//Method - 2
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+int ans; 
+int height(TreeNode* root){
+    if(!root) return 0;
+    return 1 + height(root->left);
+}
+
+void rec(TreeNode* root){
+    if(!root) return;
+    int h1 = height(root->left);
+    int h2 = height(root->right);
+    if(h1 == h2){
+        if(h2 == 1) ans = root->right->val;
+        else rec(root->right);
+    }
+    else {
+        if(h1 == 1) ans = root->left->val;
+        else rec(root->left);
+    }
+}
+ 
+int Solution::lastNode(TreeNode* A) {
+    if(height(A) == 1) return A->val;
+    rec(A);
+    return ans;
 }
