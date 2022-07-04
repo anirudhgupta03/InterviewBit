b898ad35066f348a60e9fd728e129b0a7acd06ae
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-07-04 22:40:16 +0530

    Update Points on the Straight Line.cpp

--- Hashing/Points on the Straight Line.cpp
+++ Hashing/Points on the Straight Line.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 int Solution::maxPoints(vector<int> &A, vector<int> &B) {
     
     int n = A.size();
@@ -21,4 +22,40 @@ int Solution::maxPoints(vector<int> &A, vector<int> &B) {
         points.clear();
     }
     return maxpoints + 1;
+}
+
+//Method - 2
+int Solution::maxPoints(vector<int> &A, vector<int> &B) {
+    
+    int n = A.size();
+    if(n == 0) return 0;
+    if(n == 1) return 1;
+    
+    int maxPoints = 2;
+        
+    for(int i = 0; i < n; i++){
+        
+        unordered_map<double,int> umap;
+        int x1 = A[i], y1 = B[i];  
+        double slope;
+          
+        for(int j = 0; j < n; j++){
+            
+            if(j == i) continue;
+            
+            int x2 = A[j], y2 = B[j];
+            
+            if(x1 == x2) slope = INT_MAX;
+            else slope = (double)(y2 - y1)/(x2 - x1);
+            
+            if(umap.find(slope) != umap.end()){
+                umap[slope]++;
+                maxPoints = max(maxPoints, umap[slope]);
+            }
+            else{
+                umap[slope] = 2;
+            }
+        }
+    }
+    return maxPoints;
 }
