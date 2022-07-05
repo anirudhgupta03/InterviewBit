06e9c0f56060204372e8353ffa208a9dbfb0992c
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-07-05 09:47:27 +0530

    Update Substring Concatenation.cpp

--- Hashing/Substring Concatenation.cpp
+++ Hashing/Substring Concatenation.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 vector<int> Solution::findSubstring(string A, const vector<string> &B) {
     
     int len = B[0].size();
@@ -36,6 +37,44 @@ vector<int> Solution::findSubstring(string A, const vector<string> &B) {
         if(given_words == used_words){
             res.push_back(i);
         }
+    }
+    return res;
+}
+
+//Method - 2
+vector<int> Solution::findSubstring(string A, const vector<string> &B) {
+    
+    int n = B.size();
+    vector<int> res;
+    
+    if(A.size() < n*B[0].size()){
+        return res;
+    }
+    
+    unordered_map<string,int> umap;
+    
+    for(auto &x: B){
+        umap[x]++;
+    }
+    
+    int len = n*B[0].size();
+    
+    int lo = 0, hi = len - 1;
+    
+    while(hi < A.size()){
+        unordered_map<string,int> tumap;
+        bool flag = true;
+        for(int i = lo; i <= hi; i += B[0].size()){
+            string s = A.substr(i, B[0].size());
+            tumap[s]++;
+            if(tumap[s] > umap[s]){
+                flag = false;
+                break;
+            }
+        }
+        if(flag) res.push_back(lo);
+        lo++;
+        hi++;
     }
     return res;
 }
