7a6a33da589947c15d7a27dfa366a7005c4f1b41
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-07-05 15:41:53 +0530

    Update Reverse Linked List.cpp

--- Linked List/Reverse Linked List.cpp
+++ Linked List/Reverse Linked List.cpp
@@ -1,3 +1,5 @@
+//Method - 1
+//Iterative
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -24,4 +26,41 @@ ListNode* Solution::reverseList(ListNode* A) {
     
     temp2 -> next = temp1;
     return temp2;
+}
+
+//Method - 2
+//Recursive
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ListNode* root;
+ListNode* reverse(ListNode* A){
+    
+    if(A == NULL){
+        return A;
+    }
+    ListNode* temp = reverse(A -> next);
+    A -> next = NULL;
+    
+    if(temp == NULL){
+        root = A;
+    }
+    else{
+        temp -> next = A;
+    }
+    return A;
+}
+ListNode* Solution::reverseList(ListNode* A) {
+    
+    if(A == NULL || A -> next == NULL){
+        return A;
+    }
+    root = NULL;
+    reverse(A);
+    return root;
 }
