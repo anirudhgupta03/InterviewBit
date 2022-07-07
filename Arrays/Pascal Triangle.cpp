ef45d4f66bb405d83465cccd85e82e8a27287ff1
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-07-07 15:55:26 +0530

    Update Pascal Triangle.cpp

--- Arrays/Pascal Triangle.cpp
+++ Arrays/Pascal Triangle.cpp
@@ -26,21 +26,17 @@ vector<vector<int> > Solution::solve(int A) {
 
 //Method - 2
 vector<vector<int> > Solution::solve(int A) {
-
+    
     vector<vector<int>> res;
-
     if(A == 0) return res;
     
-    vector<int> temp;
-    temp.push_back(1);
-
-    res.push_back(temp);
-
+    res.push_back({1});
+    
     for(int i = 1; i < A; i++){
         vector<int> temp;
         temp.push_back(1);
-        for(int j = 1; j <= res[i - 1].size() - 1; j++){
-            temp.push_back(res[i - 1][j - 1] + res[i - 1][j]);
+        for(int j = 1; j < i; j++){
+            temp.push_back(res[i - 1][j] + res[i - 1][j - 1]);
         }
         temp.push_back(1);
         res.push_back(temp);
