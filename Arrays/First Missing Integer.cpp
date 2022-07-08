a056ecd6372d2bb3c43cd9978c28186dc6920705
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-07-08 10:37:01 +0530

    Update First Missing Integer.cpp

--- Arrays/First Missing Integer.cpp
+++ Arrays/First Missing Integer.cpp
@@ -93,3 +93,23 @@ int Solution::firstMissingPositive(vector<int> &A) {
     }
     return n + 1;
 }
+
+//Optimal Approach 3
+int Solution::firstMissingPositive(vector<int> &A) {
+    
+    int n = A.size();
+    
+    for(int i = 0; i < n; i++){
+        if(A[i] == i + 1) continue;
+        
+        while(1){
+            if(A[i] <= 0 || A[i] > n || A[i] == A[A[i] - 1]) break;
+            swap(A[i], A[A[i] - 1]);
+        }
+    }
+    
+    for(int i = 0; i < n; i++){
+        if(A[i] != i + 1) return i + 1;
+    }
+    return n + 1;
+}
