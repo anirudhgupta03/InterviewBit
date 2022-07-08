4de9241302529c16e9c033bcc5fcecf7ea277134
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-07-08 19:23:37 +0530

    Update Move Zeroes.cpp

--- Arrays/Move Zeroes.cpp
+++ Arrays/Move Zeroes.cpp
@@ -1,14 +1,12 @@
 vector<int> Solution::solve(vector<int> &A) {
-
-    int ind = 0;
+    
+    int pos = 0;
+    
     for(int i = 0; i < A.size(); i++){
         if(A[i]){
-            A[ind] = A[i];
-            ind++;
+            swap(A[i], A[pos]);
+            pos++;
         }
-    }
-    for(int i = ind; i < A.size(); i++){
-        A[i] = 0;
     }
     return A;
 }
