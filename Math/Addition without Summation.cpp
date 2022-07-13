55ce6df04dca0860347a3bc3e23114a8e96bcca8
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-07-13 09:30:05 +0530

    Update Addition without Summation.cpp

--- Math/Addition without Summation.cpp
+++ Math/Addition without Summation.cpp
@@ -1,27 +1,41 @@
 //Method - 1
-int Solution::addNumbers(int A, int B) {
-
-    int ans = 0, c = 0, pos = 0;
-
-    while(A != 0 || B != 0){
-        int x = A % 2;
-        int y = B % 2;
-        int one = x ^ y ^ c;
-        c = (x & y) | (c & y) | (c & x);
-
-        if(one) ans |= (1 << pos);
-
-        A /= 2;
-        B /= 2;
-        pos++;
+int reverseBits(int num){
+    
+    int rev = 0;
+    while(num){
+        rev <<= 1;
+        rev |= (num & 1);
+        num >>= 1;
     }
-    if(c){
+    return rev;
+}
+int Solution::addNumbers(int A, int B) {
+    
+    int ans = 0, carry = 0, pos = 0;
+    
+    while(A || B){
+        
+        int bit1 = (A & 1);
+        int bit2 = (B & 1);
+        
+        int val = bit1 ^ bit2 ^ carry;
+        carry = (bit1 & bit2) | (bit1 & carry) | (bit2 & carry);
+        
+        if(val) ans |= (1 << pos);
+        pos++;
+        
+        A >>= 1;
+        B >>= 1;
+    }
+    
+    if(carry){
         ans |= (1 << pos);
     }
     return ans;
 }
 
 //Method - 2
+//Ref: https://www.youtube.com/watch?v=kIXhc8nZKIo
 int Solution::addNumbers(int A, int B) {
 
     while(B){
