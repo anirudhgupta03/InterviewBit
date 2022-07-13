f56228e165ab446edd29cc2d1e4baa1355a439a7
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-07-13 11:13:37 +0530

    Update Socks Pair.cpp

--- Math/Socks Pair.cpp
+++ Math/Socks Pair.cpp
@@ -1,16 +1,14 @@
 int Solution::solve(vector<int> &A) {
-
-    unordered_map<int,int> umap;
-    int count = 0;
     
-    for(int i = 0; i < A.size(); i++){
-        if(umap[A[i]]){
-            count++;
-            umap[A[i]]--;
-        }
-        else{
-            umap[A[i]]++;
-        }
+    int n = A.size();
+    vector<int> freq(n + 1, 0);
+    
+    for(auto &x: A){
+        freq[x]++;
+    }
+    int count = 0;
+    for(int i = 1; i <= n; i++){
+        count += freq[i]/2;
     }
     return count;
 }
