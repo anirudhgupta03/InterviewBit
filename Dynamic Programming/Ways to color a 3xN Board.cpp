6946a06dd3c5a4546e844b5c033a57d95440e004
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-07-22 16:06:06 +0530

    Update Ways to color a 3xN Board.cpp

--- Dynamic Programming/Ways to color a 3xN Board.cpp
+++ Dynamic Programming/Ways to color a 3xN Board.cpp
@@ -1,13 +1,44 @@
 //Ref: https://www.youtube.com/watch?v=wInXbAHi72g
+vector<vector<int>> comb;
 #define ll long long
-int Solution::solve(int A) {
-    ll mod = 1e9 + 7;
-    ll c3 = 24;
-    ll c2 = 12;
-    for(int i = 2; i <= A; i++){
-        ll temp = (7*c2 + 5*c3)%mod;
-        c3 = (10*c2 + 11*c3)%mod;
-        c2 = temp % mod;
+#define mod 1000000007
+int dp[4][4][4][100001];
+ll rec(int ind, int v1, int v2, int v3, int A){
+    
+    if(ind == A){
+        return 1;
     }
-    return (c2 + c3)%mod;
+    if(dp[v1][v2][v3][ind] != -1){
+        return dp[v1][v2][v3][ind];
+    }
+    
+    ll ans = 0;
+    for(int i = 0; i < 36; i++){
+        if(comb[i][0] != v1 && comb[i][1] != v2 && comb[i][2] != v3){
+            ans = (ans + rec(ind + 1, comb[i][0], comb[i][1], comb[i][2], A))%mod;
+        }
+    }
+    return dp[v1][v2][v3][ind] = ans;
+}
+int Solution::solve(int A) {
+    
+    comb.clear();
+    
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 4; j++){
+            for(int k = 0; k < 4; k++){
+                if(i != j && j != k){
+                    comb.push_back({i, j, k});
+                }
+            }
+        }
+    }
+    
+    memset(dp, -1, sizeof(dp));
+    
+    ll ans = 0;
+    for(int i = 0; i < 36; i++){
+        ans = (ans + rec(1, comb[i][0], comb[i][1], comb[i][2], A))%mod;
+    }
+    return ans;
 }
