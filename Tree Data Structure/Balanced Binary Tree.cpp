9d71bb6d3b3e0f8d8198945efd2cda8b5ac8178e
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-10-07 11:40:37 +0530

    Update Balanced Binary Tree.cpp

--- Tree Data Structure/Balanced Binary Tree.cpp
+++ Tree Data Structure/Balanced Binary Tree.cpp
@@ -8,39 +8,6 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-int solve(TreeNode* A, bool &flag){
-    
-    if(A == NULL){
-        return 0;
-    }
-    
-    int l = solve(A->left,flag);
-    int r = solve(A->right,flag);
-    
-    if(abs(r-l) > 1){
-        flag = false;
-    }
-    return max(l,r)+1;
-}
-int Solution::isBalanced(TreeNode* A) {
-    
-    bool flag = true;
-    
-    solve(A,flag);
-    
-    return flag;
-}
-
-//Method - 2
-/**
- * Definition for binary tree
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
 int height(TreeNode* root){
     
     if(root == NULL){
@@ -72,4 +39,64 @@ int Solution::isBalanced(TreeNode* A) {
         return true;
     }
     return false;
+}
+
+//Method - 2
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+int solve(TreeNode* A, bool &flag){
+    
+    if(A == NULL){
+        return 0;
+    }
+    
+    int l = solve(A->left,flag);
+    int r = solve(A->right,flag);
+    
+    if(abs(r-l) > 1){
+        flag = false;
+    }
+    return max(l,r)+1;
+}
+int Solution::isBalanced(TreeNode* A) {
+    
+    bool flag = true;
+    
+    solve(A,flag);
+    
+    return flag;
+}
+
+//Method - 3
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+int solve(TreeNode* A){
+    if(A == NULL){
+        return 0;
+    }
+    int lht = solve(A -> left);
+    int rht = solve(A -> right);
+    if(abs(lht - rht) > 1 || lht == -1 || rht == -1){
+        return -1;
+    }
+    return 1 + max(lht, rht);
+}
+int Solution::isBalanced(TreeNode* A) {
+    int res = solve(A);
+    if(res == -1) return 0;
+    return 1;
 }
