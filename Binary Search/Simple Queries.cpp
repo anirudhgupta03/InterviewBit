f17edf947d1a20c74745a30f176c61e6af93558d
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-10-29 11:12:49 +0530

    Update Simple Queries.cpp

--- Binary Search/Simple Queries.cpp
+++ Binary Search/Simple Queries.cpp
@@ -1,4 +1,5 @@
 //Ref: https://www.youtube.com/watch?v=gt46R1opCVw
+//Method - 1
 #define mod 1000000007
 #define ll long long
 ll findProductOfDivisors(int num){
@@ -107,6 +108,126 @@ vector<int> Solution::solve(vector<int> &A, vector<int> &B) {
             }
         }
         res.push_back(temp[ind].second);
+    }
+    return res;
+}
+
+//Method - 2
+#define mod 1000000007
+#define ll long long
+bool cmp(pair<ll,ll> &p1, pair<ll,ll> &p2){
+    return p1.first > p2.first;
+}
+vector<int> Solution::solve(vector<int> &A, vector<int> &B) {
+    
+    int n = A.size();
+    
+    vector<int> ngl(n), ngr(n);
+    
+    stack<int> st;
+    
+    ngl[0] = -1;
+    st.push(0);
+    
+    for(int i = 1; i < n; i++){
+        if(A[st.top()] > A[i]){
+            ngl[i] = st.top();
+        }
+        else{
+            while(!st.empty() && A[i] >= A[st.top()]){
+                st.pop();
+            }
+            if(st.empty()){
+                ngl[i] = -1;
+            }
+            else{
+                ngl[i] = st.top();
+            }
+        }
+        st.push(i);
+    }
+    
+    ngr[n - 1] = n;
+    while(!st.empty()){
+        st.pop();
+    }
+    st.push(n - 1);
+    
+    for(int i = n - 2; i >= 0; i--){
+        if(A[i] <= A[st.top()]){
+            ngr[i] = st.top();
+        }
+        else{
+            while(!st.empty() && A[i] > A[st.top()]){
+                st.pop();
+            }
+            if(st.empty()){
+                ngr[i] = n;
+            }
+            else{
+                ngr[i] = st.top();
+            }
+        }
+        st.push(i);
+    }
+    
+    unordered_map<int,ll> umap;
+    
+    for(int i = 0; i < n; i++){
+        umap[A[i]] += (ll)(i - ngl[i])*(ngr[i] - i);
+    }
+    
+    int maxVal = *max_element(A.begin(), A.end());
+    
+    vector<ll> prodOfDivisors(maxVal + 1);
+    
+    for(int i = 1; i <= maxVal; i++){
+        prodOfDivisors[i] = i;
+    }
+    
+    for(int i = 2; i <= maxVal; i++){
+        for(int j = 2*i; j <= maxVal; j += i){
+            prodOfDivisors[j] = (prodOfDivisors[j]*i)%mod;
+        }
+    }
+    vector<pair<ll,ll>> v;
+    
+    for(auto &x: umap){
+        v.push_back({prodOfDivisors[x.first], x.second});
+    }
+    
+    sort(v.begin(), v.end(), cmp);
+    
+    // for(int i = 0; i < v.size(); i++){
+    //     cout << v[i].first << " " << v[i].second << endl;
+    // }
+    
+    vector<ll> prefix(v.size(), 0);
+    prefix[0] = v[0].second;
+    
+    for(int i = 1; i < v.size(); i++){
+        prefix[i] = prefix[i - 1] + v[i].second;
+    }
+    
+    // for(int i = 0; i < v.size(); i++){
+    //     cout << prefix[i] << " ";
+    // }
+    // cout << endl;
+    vector<int> res;
+    
+    for(auto &query: B){
+        int lo = 0, hi = prefix.size() - 1, ans;
+        while(lo <= hi){
+            int mid = lo + (hi - lo)/2;   
+            if(prefix[mid] >= query){
+                ans = mid;
+                hi = mid - 1;
+            }
+            else{
+                lo = mid + 1;
+            }
+        }
+        res.push_back(v[ans].first);
     }
     return res;
 }
